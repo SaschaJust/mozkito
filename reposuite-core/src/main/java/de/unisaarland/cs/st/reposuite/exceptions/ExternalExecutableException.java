f748bbbc8bc2788a2cc3bb552280bfbc983e6362
Sascha Just <sascha.just@st.cs.uni-saarland.de>
2010-10-25 14:04:27 +0200

    adding missing exception class

---
+++ reposuite-core/src/main/java/de/unisaarland/cs/st/reposuite/exceptions/ExternalExecutableException.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package de.unisaarland.cs.st.reposuite.exceptions;
+
+/**
+ * @author Sascha Just <sascha.just@st.cs.uni-saarland.de>
+ * 
+ */
+public class ExternalExecutableException extends Exception {
+	
+	/**
+     * 
+     */
+	private static final long serialVersionUID = -6421711248272471659L;
+	
+	/**
+	 * 
+	 */
+	public ExternalExecutableException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @param arg0
+	 */
+	public ExternalExecutableException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public ExternalExecutableException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @param arg0
+	 */
+	public ExternalExecutableException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
