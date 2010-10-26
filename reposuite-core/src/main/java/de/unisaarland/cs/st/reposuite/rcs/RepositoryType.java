12b6e064f883f1dd0f1b85ac70dfd534550cae3a
Sascha Just <sascha.just@st.cs.uni-saarland.de>
2010-10-26 10:27:57 +0200

    fixed typo

--- reposuite-core/src/main/java/de/unisaarland/cs/st/reposuite/rcs/RepositoryType.java
+++ reposuite-core/src/main/java/de/unisaarland/cs/st/reposuite/rcs/RepositoryType.java
@@ -1,7 +1,7 @@
 package de.unisaarland.cs.st.reposuite.rcs;
 
 public enum RepositoryType {
-	SUBVERSION, GIT, MERCURIAL;
+	SUBVERSION, GIT, MERCURIAL, CVS;
 	
 	public static String getHandle() {
 		return RepositoryType.class.getSimpleName();
