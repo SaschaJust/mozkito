833d5d98939b91d07f41f81160744a25aced1f6e
Sascha Just <sascha.just@st.cs.uni-saarland.de>
2010-10-27 16:09:08 +0200

    changed Hibernate implementation to fit Miner/Jminer needs. Configuration is taken from hibernate.cfg.xml if no properties are set.

--- reposuite-core/src/main/java/de/unisaarland/cs/st/reposuite/persistence/HibernateUtil.java
+++ reposuite-core/src/main/java/de/unisaarland/cs/st/reposuite/persistence/HibernateUtil.java
@@ -1,80 +1,82 @@
 package de.unisaarland.cs.st.reposuite.persistence;
 
+import java.util.List;
 import java.util.Properties;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
+
+import de.unisaarland.cs.st.reposuite.Core;
+import de.unisaarland.cs.st.reposuite.settings.RepoSuiteSettings;
+import de.unisaarland.cs.st.reposuite.utils.ClassFinder;
+import de.unisaarland.cs.st.reposuite.utils.Logger;
 
 public class HibernateUtil {
 	
 	private static SessionFactory sessionFactory;
+	private static Session        session;
 	
-	private static SessionFactory createSessionFactory(Properties properties) {
-		AnnotationConfiguration configuration = new AnnotationConfiguration();
-		configuration.addProperties(properties);
-		//TODO Add entity classes here
-		return configuration.buildSessionFactory();
+	public static SessionFactory createSessionFactory() throws HibernateException {
+		if (sessionFactory == null) {
+			AnnotationConfiguration annotationConfiguration = new AnnotationConfiguration();
+			Properties properties = annotationConfiguration.getProperties();
+			return createSessionFactory(properties);
+		}
+		return sessionFactory;
 	}
 	
-	private static SessionFactory createSessionFactory(String host, String database, String userName, String password,
-	        String driverClassName) {
-		try {
+	private static SessionFactory createSessionFactory(Properties properties) {
+		if (sessionFactory == null) {
+			AnnotationConfiguration annotationConfiguration = new AnnotationConfiguration();
+			annotationConfiguration.setProperties(properties);
+			List<Class<?>> annotatedClasses;
 			
-			String url = "";
-			if (driverClassName.toLowerCase().contains("postgresql")) {
-				url = "jdbc:mysql://" + host + "/" + database + "?useUnicode=true&characterEncoding=UTF-8";
-			} else if (driverClassName.toLowerCase().contains("mysql")) {
-				url = "jdbc:postgresql://" + host + "/" + database + "?useUnicode=true&characterEncoding=UTF-8";
+			try {
+				annotatedClasses = ClassFinder.getClassesExtendingClass(Core.class.getPackage(), Annotated.class);
+			} catch (Exception e) {
+				if (RepoSuiteSettings.logError()) {
+					Logger.error(e.getMessage(), e);
+				}
+				throw new RuntimeException();
 			}
 			
-			Properties properties = getDefaultProperties();
+			for (Class<?> clazz : annotatedClasses) {
+				annotationConfiguration.addAnnotatedClass(clazz);
+			}
+			
+			sessionFactory = annotationConfiguration.configure().buildSessionFactory();
+		}
+		return sessionFactory;
+	}
+	
+	public static SessionFactory createSessionFactory(String host, String database, String user, String password,
+	        String type, String driver) throws HibernateException {
+		try {
+			String url = "jdbc:" + type.toLowerCase() + "://" + host + "/" + database
+			        + "?useUnicode=true&characterEncoding=UTF-8";
+			
+			Properties properties = new Properties();
 			properties.put("hibernate.connection.url", url);
-			properties.put("hibernate.connection.driver_class", driverClassName);
-			properties.put("hibernate.connection.username", userName);
+			properties.put("hibernate.hbm2ddl.auto", "update");
+			properties.put("hibernate.connection.autocommit", "false");
+			properties.put("hibernate.show_sql", "false");
+			properties.put("hibernate.connection.driver_class", driver);
+			properties.put("hibernate.connection.username", user);
 			properties.put("hibernate.connection.password", password);
+			
 			return createSessionFactory(properties);
 		} catch (Throwable ex) {
 			throw new ExceptionInInitializerError(ex);
 		}
 	}
 	
-	public static Properties getDefaultProperties() {
-		Properties properties = new Properties();
-		properties.put("hibernate.connection.url", "");
-		properties.put("hibernate.hbm2ddl.auto", "update");
-		properties.put("hibernate.connection.autocommit", "false");
-		properties.put("hibernate.show_sql", "false");
-		properties.put("hibernate.connection.driver_class", "org.postgresql.Driver");
-		properties.put("hibernate.connection.username", "");
-		properties.put("hibernate.connection.password", "");
-		return properties;
-	}
-	
-	public static SessionFactory getMYSQLSessionFactory(String host, String database, String userName, String password) {
-		SessionFactory factory = createSessionFactory(host, database, userName, password, "org.postgresql.Driver");
-		sessionFactory = factory;
-		return factory;
-	}
-	
-	public static SessionFactory getPSQLSessionFactory(String host, String database, String userName, String password) {
-		SessionFactory factory = createSessionFactory(host, database, userName, password, "com.mysql.jdbc.Driver");
-		sessionFactory = factory;
-		return factory;
-	}
-	
-	@Deprecated
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-	
-	public static SessionFactory getSessionFactory(Properties properties) {
-		SessionFactory factory = createSessionFactory(properties);
-		sessionFactory = factory;
-		return factory;
-	}
-	
 	public static void shutdown() {
+		session.close();
 		sessionFactory.close();
+		session = null;
+		sessionFactory = null;
 	}
 	
 }
