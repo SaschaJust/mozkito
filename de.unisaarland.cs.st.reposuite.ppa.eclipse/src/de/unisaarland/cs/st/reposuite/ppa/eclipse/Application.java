3bf51775635748cf504903b443c7c1ae0a3f7b37
Kim Herzig <kim@cs.uni-saarland.de>
2011-01-25 12:05:41 +0100

    Adding eclipse plugin that allows to run reposuite-ppa within equinox environment

---
+++ de.unisaarland.cs.st.reposuite.ppa.eclipse/src/de/unisaarland/cs/st/reposuite/ppa/eclipse/Application.java
@@ -0,0 +1,49 @@
+package de.unisaarland.cs.st.reposuite.ppa.eclipse;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+import de.unisaarland.cs.st.reposuite.utils.FileUtils;
+
+/**
+ * This class controls all aspects of the application's execution
+ */
+public class Application implements IApplication {
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.
+	 * IApplicationContext)
+	 */
+	@Override
+	public Object start(final IApplicationContext context) throws Exception {
+		
+		String baseDir = System.getProperty("user.home") + FileUtils.fileSeparator + ".m2" + FileUtils.fileSeparator
+		+ "repository" + FileUtils.fileSeparator + "de" + FileUtils.fileSeparator + "unisaarland"
+		+ FileUtils.fileSeparator + "cs" + FileUtils.fileSeparator + "st" + FileUtils.fileSeparator
+		+ "reposuite";
+		
+		String utils = baseDir + "-utils" + FileUtils.fileSeparator + "0.1-SNAPSHOT" + FileUtils.fileSeparator
+		+ "reposuite-utils-0.1-SNAPSHOT.jar";
+		String core = baseDir + "-core" + FileUtils.fileSeparator + "0.1-SNAPSHOT" + FileUtils.fileSeparator
+		+ "reposuite-core-0.1-SNAPSHOT.jar";
+		String ppa = baseDir + "-ppa" + FileUtils.fileSeparator + "0.1-SNAPSHOT" + FileUtils.fileSeparator
+		+ "reposuite-ppa-0.1-SNAPSHOT.jar";
+		
+		System.setProperty("reposuiteClassLookup", utils + ":" + core + ":" + ppa);
+		
+		PPA.Main.main(new String[0]);
+		return IApplication.EXIT_OK;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.equinox.app.IApplication#stop()
+	 */
+	@Override
+	public void stop() {
+		// nothing to do
+	}
+}
