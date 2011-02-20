74223056ebf1d493426e52d3cff8ef01647ce7e1
friedrich.benjamin@gmail.com <friedrich.benjamin@gmail.com>
2011-02-20 22:49:30 +0000

    added Javadoc comments to IEMineEventBusListener

--- moskito-emine/org.se2010.emine/src/org/se2010/emine/events/IEMineEventListener.java
+++ moskito-emine/org.se2010.emine/src/org/se2010/emine/events/IEMineEventListener.java
@@ -1,7 +1,23 @@
 package org.se2010.emine.events;
 
+/**
+ * Interface for components listening on {@link IEMineEventBus} for specific {@link IEMineEvent}s.
+ * 
+ * @author   Benjamin Friedrich (<a href="mailto:friedrich.benjamin@gmail.com">friedrich.benjamin@gmail.com</a>)
+ * @version  1.0 02/2011
+ */
 public interface IEMineEventListener 
 {
-	// should be synchronized!
+	/**
+	 * onEvent() is called by the {@link IEMineEventBus} instance
+	 * on which the listener is registered when a corresponding
+	 * {@link IEMineEvent} is fired via {@link IEMineEventBus#fireEvent(IEMineEvent)}.
+	 * 
+	 * <b>Note:</b> Every implementation of {@link IEMineEventListener} should
+	 * consider thread safety regarding this method as it is usually called
+	 * concurrently.
+	 * 
+	 * @param event  {@link IEMineEvent} instance for which the listener is registered on {@link IEMineEventBus}
+	 */
 	public void onEvent(IEMineEvent event);
 }
