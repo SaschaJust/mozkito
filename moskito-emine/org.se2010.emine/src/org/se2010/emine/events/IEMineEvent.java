87faf4ff83445283d5f3c9910bf02df4a4e5d45c
friedrich.benjamin@gmail.com <friedrich.benjamin@gmail.com>
2011-02-20 22:24:44 +0000

    added Javadoc comments to IEMineEvent

--- moskito-emine/org.se2010.emine/src/org/se2010/emine/events/IEMineEvent.java
+++ moskito-emine/org.se2010.emine/src/org/se2010/emine/events/IEMineEvent.java
@@ -1,5 +1,11 @@
 package org.se2010.emine.events;
 
+/**
+ * Marker interface for all eMine related events.
+ * 
+ * @author   Benjamin Friedrich (<a href="mailto:friedrich.benjamin@gmail.com">friedrich.benjamin@gmail.com</a>)
+ * @version  1.0 02/2011
+ */
 public interface IEMineEvent 
 {
 }
