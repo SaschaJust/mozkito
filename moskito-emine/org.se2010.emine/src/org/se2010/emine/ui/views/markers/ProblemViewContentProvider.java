010815513c9a6c43704f7e8dd71c191081a3dcea
kaushik.puzo@gmail.com <kaushik.puzo@gmail.com>
2011-02-25 11:23:01 +0000

    Fixes

--- moskito-emine/org.se2010.emine/src/org/se2010/emine/ui/views/markers/ProblemViewContentProvider.java
+++ moskito-emine/org.se2010.emine/src/org/se2010/emine/ui/views/markers/ProblemViewContentProvider.java
@@ -8,11 +8,34 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Control;
 import org.se2010.emine.artifacts.ProblemArtifact;
 import org.se2010.emine.artifacts.ProblemArtifactTypeList;
+import org.se2010.emine.events.EMineEventBus;
+import org.se2010.emine.events.IEMineEvent;
+import org.se2010.emine.events.IEMineEventListener;
+import org.se2010.emine.events.reposuite.RepoSuiteEvent;
 
-public class ProblemViewContentProvider implements ITreeContentProvider {
+public class ProblemViewContentProvider implements ITreeContentProvider,IEMineEventListener {
+	
+	  private RepoSuiteEvent event;
+	  	private List< ProblemArtifactTypeList> input = null;
+
+	  
+		private static ArrayList<ProblemArtifactTypeList> display = new ArrayList<ProblemArtifactTypeList>();
+		  private StructuredViewer viewer;
+		  
+			private static Object[] EMPTY_ARRAY = new Object[0];
+
+		  
+		  
+		  public ProblemViewContentProvider() {
+			  EMineEventBus.getInstance().registerEventListener(RepoSuiteEvent.class, this);	
+
+		  }
+
 
 	@Override
 	public void dispose() {
@@ -21,10 +44,23 @@ public class ProblemViewContentProvider implements ITreeContentProvider {
 	}
 
 	@Override
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		// TODO Auto-generated method stub
-
-	}
+	  public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		  
+		  if (viewer == null){
+			  
+			  this.viewer = (StructuredViewer) v;	
+			  
+		  if (input == null && newInput !=null){
+			  
+			  input = (List) newInput;
+		  }
+		  
+		  if (newInput == null && input != null) {
+		      input = null;
+		    }
+		  
+		  }  
+	  }
 
 	@Override
 	public Object[] getChildren(Object parentElement) {
@@ -46,9 +82,56 @@ public class ProblemViewContentProvider implements ITreeContentProvider {
 			return (!((List) element).isEmpty());
 		return false;
 	}
+	
+	  /** 
+	   * Obtains content from the input in the form of an array of <code>IMarker</code>
+	   * elements.
+	   * 
+	   * @return Object[] - Array of <code>IMarker</code> objects from the workspace 
+	   * 
+	   * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+	   */
+//	  public Object[] getElements(Object parent) {
+//	    
+//		  if (event !=null)
+//	 
+//		 return  display.toArray();
+//		  
+//		  else 
+//			  
+//			 return EMPTY_ARRAY;
+//	  }
+
+	  
+
+	public Object[] getElements(Object inputElement) {
+		
+		return getChildren(display);
+	}
 
 	@Override
-	public Object[] getElements(Object inputElement) {
-		return getChildren(inputElement);
+	public void onEvent(IEMineEvent event) {
+
+		// TODO Auto-generated method stub	 input = ((RepoSuiteEvent)event).getArtifact();	
+		 ArrayList<ProblemArtifactTypeList> eventArtifacts = ((RepoSuiteEvent)event).getArtifactTypeList();
+		 //Control ctrl = viewer.getControl();
+		 this.event = ((RepoSuiteEvent)event);
+		 for (int i = 0 ; i < eventArtifacts.size() ; i++){
+			 
+			ProblemArtifactTypeList p = eventArtifacts.get(i);
+			 display.add(p);
+		 }
+		 
+		 
+		 Control ctrl = viewer.getControl();
+		    if (ctrl != null && !ctrl.isDisposed()) {     
+		        ctrl.getDisplay().asyncExec(new Runnable() {
+		          public void run() {
+		        	  viewer.refresh();
+		            //  viewer.refresh();  
+		          }
+		        }); 
+		    }	
+		
 	}
 }
