df72ee7fdd775e3855293aa2baf7af6c0df8db53
Kim Herzig <kim@cs.uni-saarland.de>
2011-03-16 10:38:32 +0100

    starting to implement untangling (as standalone tool first)

---
+++ reposuite-untangling/src/main/java/untangling/Standalone.java
@@ -0,0 +1,26 @@
+package untangling;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import de.unisaarland.cs.st.reposuite.ppa.model.JavaChangeOperation;
+import de.unisaarland.cs.st.reposuite.utils.specification.NoneNull;
+
+public class Standalone {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@NoneNull
+	public static Set<Set<JavaChangeOperation>> untangle(final Set<JavaChangeOperation> blob) {
+		Set<Set<JavaChangeOperation>> result = new HashSet<Set<JavaChangeOperation>>();
+		// TODO
+		return result;
+	}
+	
+}
