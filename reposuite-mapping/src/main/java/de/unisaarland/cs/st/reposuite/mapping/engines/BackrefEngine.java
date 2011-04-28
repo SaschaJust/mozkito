0dc94832ba8cd9fc88b61398f2df2f932d80ecf2
Sascha Just <sascha.just@own-hero.net>
2011-04-28 16:36:18 +0200

    adding BackRefEngine stub. if we find a backreference in the comment of a report, we can use this as a feature as well...

---
+++ reposuite-mapping/src/main/java/de/unisaarland/cs/st/reposuite/mapping/engines/BackrefEngine.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package de.unisaarland.cs.st.reposuite.mapping.engines;
+
+import de.unisaarland.cs.st.reposuite.mapping.settings.MappingSettings;
+
+
+/**
+ * @author Sascha Just <sascha.just@st.cs.uni-saarland.de>
+ *
+ */
+public class BackrefEngine extends MappingEngine {
+	
+	/**
+	 * @param settings
+	 */
+	public BackrefEngine(MappingSettings settings) {
+		super(settings);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
