2b6c5c5694adf9627518b933df36f96bda4bcaeb
Sascha Just <sascha.just@own-hero.net>
2011-04-30 10:07:16 +0200

    splitting up search engine to Summary-/Description-/CommentSearchEngine

---
+++ reposuite-mapping/src/main/java/de/unisaarland/cs/st/reposuite/mapping/engines/CommentSearchEngine.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package de.unisaarland.cs.st.reposuite.mapping.engines;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.util.Version;
+
+import de.unisaarland.cs.st.reposuite.bugs.tracker.model.Report;
+import de.unisaarland.cs.st.reposuite.exceptions.UnrecoverableError;
+import de.unisaarland.cs.st.reposuite.mapping.model.MapScore;
+import de.unisaarland.cs.st.reposuite.mapping.settings.MappingSettings;
+import de.unisaarland.cs.st.reposuite.rcs.model.RCSTransaction;
+
+/**
+ * @author Sascha Just <sascha.just@st.cs.uni-saarland.de>
+ *
+ */
+public class CommentSearchEngine extends SearchEngine {
+	
+	private QueryParser parser = null;
+	
+	/**
+	 * 
+	 */
+	public CommentSearchEngine(final MappingSettings settings) {
+		super(settings);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * de.unisaarland.cs.st.reposuite.mapping.engines.MappingEngine#score(de
+	 * .unisaarland.cs.st.reposuite.rcs.model.RCSTransaction,
+	 * de.unisaarland.cs.st.reposuite.bugs.tracker.model.Report,
+	 * de.unisaarland.cs.st.reposuite.mapping.model.MapScore)
+	 */
+	@Override
+	public void score(final RCSTransaction transaction,
+	                  final Report report,
+	                  final MapScore score) {
+		try {
+			this.parser = new QueryParser(Version.LUCENE_31, "comment", getStorage().getAnalyzer());
+			Query query = buildQuery(transaction.getMessage(), this.parser);
+			
+			ScoreDoc[] hits = getStorage().getIsearcherReports().search(query, null, 1000).scoreDocs;
+			// Iterate through the results:
+			for (ScoreDoc hit : hits) {
+				Document hitDoc = getStorage().getIsearcherReports().doc(hit.doc);
+				Long bugId = Long.parseLong(hitDoc.get("bugid"));
+				
+				if (bugId.compareTo(report.getId()) == 0) {
+					score.addFeature(hit.score, "message", truncate(transaction.getMessage()), "comment",
+					                 truncate(report.getSummary()), this.getClass());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			throw new UnrecoverableError(e);
+		}
+	}
+	
+}
--- reposuite-mapping/src/main/java/de/unisaarland/cs/st/reposuite/mapping/engines/SearchEngine.java
+++ reposuite-mapping/src/main/java/de/unisaarland/cs/st/reposuite/mapping/engines/SearchEngine.java
@@ -4,37 +4,21 @@
 package de.unisaarland.cs.st.reposuite.mapping.engines;
 
 import java.lang.reflect.Constructor;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 
-import de.unisaarland.cs.st.reposuite.bugs.tracker.model.Comment;
-import de.unisaarland.cs.st.reposuite.bugs.tracker.model.Report;
 import de.unisaarland.cs.st.reposuite.exceptions.UnrecoverableError;
-import de.unisaarland.cs.st.reposuite.mapping.model.MapScore;
 import de.unisaarland.cs.st.reposuite.mapping.settings.MappingArguments;
 import de.unisaarland.cs.st.reposuite.mapping.settings.MappingSettings;
-import de.unisaarland.cs.st.reposuite.persistence.Criteria;
-import de.unisaarland.cs.st.reposuite.persistence.PersistenceUtil;
-import de.unisaarland.cs.st.reposuite.rcs.model.RCSTransaction;
+import de.unisaarland.cs.st.reposuite.mapping.storages.LuceneStorage;
+import de.unisaarland.cs.st.reposuite.mapping.storages.MappingStorage;
 import de.unisaarland.cs.st.reposuite.settings.LongArgument;
 import de.unisaarland.cs.st.reposuite.settings.StringArgument;
 
@@ -42,15 +26,9 @@ import de.unisaarland.cs.st.reposuite.settings.StringArgument;
  * @author Sascha Just <sascha.just@st.cs.uni-saarland.de>
  *
  */
-public class SearchEngine extends MappingEngine {
+public abstract class SearchEngine extends MappingEngine {
 	
-	private Analyzer                      analyzer         = new EnglishAnalyzer(Version.LUCENE_31);
-	private final Directory               reportDirectory  = new RAMDirectory();
-	private IndexWriter                   iwriterReports   = null;
-	private QueryParser                   parser           = null;
-	
-	private final HashMap<Long, Document> reportDocuments  = new HashMap<Long, Document>();
-	private IndexSearcher                 isearcherReports = null;
+	private LuceneStorage storage;
 	
 	/**
 	 * @param settings
@@ -60,32 +38,12 @@ public class SearchEngine extends MappingEngine {
 	}
 	
 	/**
-	 * @param report
-	 */
-	private void addReportDocument(final Report report) {
-		Document doc = new Document();
-		doc.add(new Field("summary", report.getSummary(), Field.Store.YES, Field.Index.ANALYZED));
-		doc.add(new Field("description", report.getDescription(), Field.Store.YES, Field.Index.ANALYZED));
-		doc.add(new Field("bugid", "" + report.getId(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-		for (Comment comment : report.getComments()) {
-			doc.add(new Field("comment" + comment.getId(), "" + comment.getMessage(), Field.Store.YES,
-			                  Field.Index.ANALYZED));
-		}
-		this.reportDocuments.put(report.getId(), doc);
-		try {
-			this.iwriterReports.addDocument(doc);
-		} catch (Exception e) {
-			throw new UnrecoverableError(e);
-		}
-	}
-	
-	/**
 	 * @param queryString
 	 * @param queryParser
 	 * @return
 	 */
-	private Query buildQuery(String queryString,
-	                         final QueryParser queryParser) {
+	protected Query buildQuery(String queryString,
+	                           final QueryParser queryParser) {
 		Query query = null;
 		queryString = queryString.replaceAll("[^a-zA-Z0-9]", " ");
 		
@@ -109,6 +67,13 @@ public class SearchEngine extends MappingEngine {
 		return query;
 	}
 	
+	/**
+	 * @return
+	 */
+	public LuceneStorage getStorage() {
+		return this.storage;
+	}
+	
 	/*
 	 * (non-Javadoc)
 	 * @see de.unisaarland.cs.st.reposuite.mapping.engines.MappingEngine#init()
@@ -116,40 +81,29 @@ public class SearchEngine extends MappingEngine {
 	@Override
 	public void init() {
 		super.init();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * de.unisaarland.cs.st.reposuite.mapping.engines.MappingEngine#provideStorage
+	 * (de.unisaarland.cs.st.reposuite.mapping.storages.MappingStorage)
+	 */
+	@Override
+	public void provideStorage(final MappingStorage storage) {
+		super.provideStorage(storage);
+		this.storage = getStorage(LuceneStorage.class);
 		String value = (String) getSettings().getSetting("mapping.config.language").getValue();
 		String[] split = value.split(":");
 		try {
-			Class<?> clazz = Class.forName("org.apache.lucene.analysis." + split[0] + "." + split[1] + "Analyzer");
-			Constructor<?> constructor = clazz.getConstructor(Version.class);
-			Analyzer newInstance = (Analyzer) constructor.newInstance(Version.LUCENE_31);
-			this.analyzer = newInstance;
-			IndexWriterConfig indexWriterConfig = new IndexWriterConfig(Version.LUCENE_31, this.analyzer);
-			this.iwriterReports = new IndexWriter(this.reportDirectory, indexWriterConfig);
-		} catch (Exception e) {
-			throw new UnrecoverableError(e);
-		}
-		super.init();
-	}
-	
-	@Override
-	public void loadData(final PersistenceUtil util) {
-		Criteria<Report> criteria = util.createCriteria(Report.class);
-		List<Report> list = util.load(criteria);
-		for (Report report : list) {
-			addReportDocument(report);
-		}
-		try {
-			this.iwriterReports.close();
-		} catch (Exception e) {
-			throw new UnrecoverableError(e);
-		} finally {
-			try {
-				this.isearcherReports = new IndexSearcher(this.reportDirectory, true);
-			} catch (Exception e) {
-				throw new UnrecoverableError(e);
+			if (this.storage.getAnalyzer() == null) {
+				Class<?> clazz = Class.forName("org.apache.lucene.analysis." + split[0] + "." + split[1] + "Analyzer");
+				Constructor<?> constructor = clazz.getConstructor(Version.class);
+				Analyzer newInstance = (Analyzer) constructor.newInstance(Version.LUCENE_31);
+				this.storage.setAnalyzer(newInstance);
 			}
-			this.parser = new QueryParser(Version.LUCENE_31, "content", this.analyzer);
-			BooleanQuery.setMaxClauseCount(102400);
+		} catch (Exception e) {
+			throw new UnrecoverableError(e);
 		}
 	}
 	
@@ -175,37 +129,18 @@ public class SearchEngine extends MappingEngine {
 	
 	/*
 	 * (non-Javadoc)
-	 * @see
-	 * de.unisaarland.cs.st.reposuite.mapping.engines.MappingEngine#score(de
-	 * .unisaarland.cs.st.reposuite.rcs.model.RCSTransaction,
-	 * de.unisaarland.cs.st.reposuite.bugs.tracker.model.Report,
-	 * de.unisaarland.cs.st.reposuite.mapping.model.MapScore)
+	 * @see de.unisaarland.cs.st.reposuite.mapping.engines.MappingEngine#
+	 * storageDependency()
 	 */
 	@Override
-	public void score(final RCSTransaction transaction,
-	                  final Report report,
-	                  final MapScore score) {
-		super.score(transaction, report, score);
-		
-		try {
-			this.parser = new QueryParser(Version.LUCENE_31, "content", this.analyzer);
-			Query query = buildQuery(transaction.getMessage(), this.parser);
-			this.isearcherReports = new IndexSearcher(this.reportDirectory, true); // read-only=true
-			// Parse a simple query that searches for "text":
+	public Set<Class<? extends MappingStorage>> storageDependency() {
+		return new HashSet<Class<? extends MappingStorage>>() {
 			
-			ScoreDoc[] hits = this.isearcherReports.search(query, null, 1000).scoreDocs;
-			// Iterate through the results:
-			for (ScoreDoc hit : hits) {
-				Document hitDoc = this.isearcherReports.doc(hit.doc);
-				Long bugId = Long.parseLong(hitDoc.get("bugid"));
-				
-				if (bugId.compareTo(report.getId()) == 0) {
-					score.addFeature(hit.score, "message", transaction.getMessage(), this.getClass());
-					break;
-				}
+			private static final long serialVersionUID = 8360079171674014391L;
+			
+			{
+				add(LuceneStorage.class);
 			}
-		} catch (Exception e) {
-			throw new UnrecoverableError(e);
-		}
+		};
 	}
 }
