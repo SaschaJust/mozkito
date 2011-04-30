c846b1c6c7287f86db05666b92a4cf2e93017d71
Sascha Just <sascha.just@own-hero.net>
2011-04-30 09:59:49 +0200

    ensuring URIArguments provide a scheme. This will break reposuite-rcs. See bug #179

--- reposuite-utils/src/main/java/de/unisaarland/cs/st/reposuite/settings/URIArgument.java
+++ reposuite-utils/src/main/java/de/unisaarland/cs/st/reposuite/settings/URIArgument.java
@@ -1,10 +1,12 @@
 package de.unisaarland.cs.st.reposuite.settings;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import de.unisaarland.cs.st.reposuite.exceptions.Shutdown;
 import de.unisaarland.cs.st.reposuite.utils.Logger;
+import de.unisaarland.cs.st.reposuite.utils.Regex;
 
 /**
  * @author Kim Herzig <herzig@cs.uni-saarland.de>
@@ -42,8 +44,94 @@ public class URIArgument extends RepoSuiteArgument {
 			return null;
 		}
 		
+		URI uri = null;
+		// see RFC2396
+		// String lowalpha = "a-z";
+		// String upalpha = "A-Z";
+		// String alpha = lowalpha + upalpha;
+		// String digit = "0-9";
+		// String alphanum = alpha + digit;
+		// String reserved = ";/?:@&=+$,";
+		// String mark = "-_.!Z*'()";
+		// String unreserved = alphanum + mark;
+		// String hex = digit + "A-Fa-f";
+		// String escaped = "%" + "[" + hex + "]{2}";
+		// String delims = "<>#%\"";
+		// String unwise = "{}|\\\\^\\[\\]`";
+		//
+		// String reg_name = "[" + unreserved + escaped + "$,;:@&=+]+";
+		// String userinfo = unreserved + escaped + ";:&=+$,";
+		// String port = "[" + digit + "]*";
+		// String domainlabel = "([" + alphanum + "]|[" + alphanum + "][" +
+		// alphanum + "-]*["+ alphanum + "])";
+		// String toplabel = "([" + alpha + "]|[" + alpha + "][" + alphanum +
+		// "-]*["+ alphanum + "])";
+		// String hostname = "(" + domainlabel + "\\.)*" + toplabel+ "\\.?";
+		// String ipv4address = "[" + digit + "]+\\." + "[" + digit + "]+\\." +
+		// "[" + digit + "]+\\." + "[" + digit + "]+";
+		// String host = "({host}" + hostname + "|" + ipv4address + ")";
+		// String hostport = host + "(:" + port +")?";
+		// String server = "[" + userinfo +"]?" + hostport;
+		//
+		// String authority = "({authority}" + server + reg_name + ")";
+		// String scheme = "({scheme}[" + alpha + "]" + "[" + alpha + digit +
+		// "+-." + "])";
+		// String path = "({path}" + abs_path + "|" + opaque_part + ")";
+		// String query = "({query})";
+		//
+		// Regex uriRegex = new Regex(scheme + ":" +authority+path+"\\?" +query
+		// );
+		
+		String err = null;
+		Regex uriRegex = new Regex(
+		                           "^(({scheme}[^:/?#]+):)?(//({authority}[^/?#]*))?({path}[^?#]*)(\\?({query}[^#]*))?(#({fragment}.*))?");
 		try {
-			return new URI(this.stringValue);
+			if (uriRegex.find(this.stringValue) == null) {
+				err = "URI does not match regex: " + uriRegex.getPattern();
+			} else {
+				if (uriRegex.getGroup("scheme") == null) {
+					
+					if (Logger.logWarn()) {
+						Logger.warn("Scheme missing when parsing URI:" + this.stringValue + " Guessing scheme: file.");
+					}
+					
+					if (uriRegex.getGroup("authority") == null) {
+						if (uriRegex.getGroup("path") != null) {
+							// guess file
+							File file = new File(uriRegex.getGroup("path"));
+							if (file.exists() && file.canRead()) {
+								
+								if (Logger.logInfo()) {
+									Logger.info("Found readable " + (file.isDirectory()
+									                                                   ? "directory"
+									                                                   : "file") + " at location: "
+									        + file.getAbsolutePath());
+								}
+								uri = new URI("file", null, file.getAbsolutePath(), null);
+							} else {
+								err = "Local path does not reference an existing, readable file/dir: "
+								        + file.getAbsolutePath();
+							}
+						} else {
+							err = "`path` part of the URI is not set: " + this.stringValue;
+						}
+					} else {
+						err = "`authority` part of the URI is set, but scheme is missing: " + this.stringValue;
+					}
+				} else {
+					uri = new URI(this.stringValue);
+				}
+			}
+			
+			if (err != null) {
+				if (Logger.logError()) {
+					Logger.error("When parsing URI string `" + this.stringValue + "` for argument `" + getName()
+					        + "`, the following error occurred: " + err);
+				}
+				throw new Shutdown();
+			} else {
+				return uri;
+			}
 		} catch (URISyntaxException e) {
 			if (Logger.logError()) {
 				Logger.error("When parsing URI string `" + this.stringValue + "` for argument `" + getName()
