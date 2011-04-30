6902a9ea1923e6e261985464f7da9d9567f3c6cc
Sascha Just <sascha.just@own-hero.net>
2011-04-30 10:01:18 +0200

    Adding missing IOException annotation to stream dependent interface

--- reposuite-utils/src/main/java/de/unisaarland/cs/st/reposuite/output/Displayable.java
+++ reposuite-utils/src/main/java/de/unisaarland/cs/st/reposuite/output/Displayable.java
@@ -3,6 +3,7 @@
  */
 package de.unisaarland.cs.st.reposuite.output;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 import org.dom4j.Document;
@@ -16,21 +17,21 @@ public interface Displayable {
 	
 	public String toCSV();
 	
-	public void toCSV(OutputStream stream);
+	public void toCSV(OutputStream stream) throws IOException;
 	
 	public HTMLDocument toHTML();
 	
-	public void toHTML(OutputStream stream);
+	public void toHTML(OutputStream stream) throws IOException;
 	
 	public String toTerm();
 	
-	public void toTerm(OutputStream stream);
+	public void toTerm(OutputStream stream) throws IOException;
 	
 	public String toText();
 	
-	public void toText(OutputStream stream);
+	public void toText(OutputStream stream) throws IOException;
 	
 	public Document toXML();
 	
-	public void toXML(OutputStream stream);
+	public void toXML(OutputStream stream) throws IOException;
 }
