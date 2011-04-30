068de6b88c06a72726fba1c0e72d4eb26a29970c
Sascha Just <sascha.just@own-hero.net>
2011-04-30 10:08:09 +0200

    adding first version of backrefengine, checking for backreferences to the transaction in the report's comments

--- reposuite-mapping/src/main/java/de/unisaarland/cs/st/reposuite/mapping/engines/BackrefEngine.java
+++ reposuite-mapping/src/main/java/de/unisaarland/cs/st/reposuite/mapping/engines/BackrefEngine.java
@@ -3,11 +3,13 @@
  */
 package de.unisaarland.cs.st.reposuite.mapping.engines;
 
+import de.unisaarland.cs.st.reposuite.bugs.tracker.model.Comment;
 import de.unisaarland.cs.st.reposuite.bugs.tracker.model.Report;
 import de.unisaarland.cs.st.reposuite.mapping.model.MapScore;
 import de.unisaarland.cs.st.reposuite.mapping.settings.MappingArguments;
 import de.unisaarland.cs.st.reposuite.mapping.settings.MappingSettings;
 import de.unisaarland.cs.st.reposuite.rcs.model.RCSTransaction;
+import de.unisaarland.cs.st.reposuite.settings.DoubleArgument;
 
 /**
  * @author Sascha Just <sascha.just@st.cs.uni-saarland.de>
@@ -15,33 +17,85 @@ import de.unisaarland.cs.st.reposuite.rcs.model.RCSTransaction;
  */
 public class BackrefEngine extends MappingEngine {
 	
+	private double scoreBackRef;
+	
 	/**
 	 * @param settings
 	 */
 	public BackrefEngine(final MappingSettings settings) {
 		super(settings);
-		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * @return the scoreBackRef
+	 */
+	public double getScoreBackRef() {
+		return this.scoreBackRef;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see de.unisaarland.cs.st.reposuite.mapping.engines.MappingEngine#init()
+	 */
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
 		super.init();
+		setScoreBackRef((Double) getSettings().getSetting("mapping.score.BackRef").getValue());
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * de.unisaarland.cs.st.reposuite.mapping.engines.MappingEngine#register
+	 * (de.unisaarland.cs.st.reposuite.mapping.settings.MappingSettings,
+	 * de.unisaarland.cs.st.reposuite.mapping.settings.MappingArguments,
+	 * boolean)
+	 */
 	@Override
 	public void register(final MappingSettings settings,
 	                     final MappingArguments arguments,
 	                     final boolean isRequired) {
-		// TODO Auto-generated method stub
 		super.register(settings, arguments, isRequired);
+		arguments.addArgument(new DoubleArgument(settings, "mapping.score.BackRef",
+		                                         "Score for backreference in transaction and report.", "0.5",
+		                                         isRequired));
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * de.unisaarland.cs.st.reposuite.mapping.engines.MappingEngine#score(de
+	 * .unisaarland.cs.st.reposuite.rcs.model.RCSTransaction,
+	 * de.unisaarland.cs.st.reposuite.bugs.tracker.model.Report,
+	 * de.unisaarland.cs.st.reposuite.mapping.model.MapScore)
+	 */
 	@Override
 	public void score(final RCSTransaction transaction,
 	                  final Report report,
 	                  final MapScore score) {
-		// TODO Auto-generated method stub
-		super.score(transaction, report, score);
+		if (report.getDescription().contains(transaction.getId())) {
+			score.addFeature(getScoreBackRef(), "id", transaction.getId(), "description",
+			                 truncate(report.getDescription()), this.getClass());
+		} else {
+			boolean found = false;
+			for (Comment comment : report.getComments()) {
+				if (comment.getMessage().contains(transaction.getId())) {
+					found = true;
+					score.addFeature(getScoreBackRef(), "id", transaction.getId(), "comment" + comment.getId()
+					        + ":message", truncate(comment.getMessage()), this.getClass());
+					break;
+				}
+			}
+			if (!found) {
+				score.addFeature(0, "id", transaction.getId(), "report", "description|comments", this.getClass());
+			}
+		}
+	}
+	
+	/**
+	 * @param scoreBackRef the scoreBackRef to set
+	 */
+	public void setScoreBackRef(final double scoreBackRef) {
+		this.scoreBackRef = scoreBackRef;
 	}
 }
