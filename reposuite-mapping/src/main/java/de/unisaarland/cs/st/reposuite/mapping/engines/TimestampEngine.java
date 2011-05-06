639cccfb0f444b9955d33968f0bd2a03d0049511
Sascha Just <sascha.just@own-hero.net>
2011-05-06 02:59:42 +0200

    Fixing TimestampEngine according to 1/1+d scheme.
    adding new CreationOrderEngine
    
    adding new CompletedOrderEngine
    
    renamed AuthorEngine to AuthorEqualityEngine

--- reposuite-mapping/src/main/java/de/unisaarland/cs/st/reposuite/mapping/engines/TimestampEngine.java
+++ reposuite-mapping/src/main/java/de/unisaarland/cs/st/reposuite/mapping/engines/TimestampEngine.java
@@ -3,7 +3,6 @@
  */
 package de.unisaarland.cs.st.reposuite.mapping.engines;
 
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -14,13 +13,15 @@ import net.ownhero.dev.kanuni.checks.CollectionCheck;
 import org.joda.time.Interval;
 
 import de.unisaarland.cs.st.reposuite.bugs.tracker.elements.Resolution;
+import de.unisaarland.cs.st.reposuite.bugs.tracker.model.History;
+import de.unisaarland.cs.st.reposuite.bugs.tracker.model.HistoryElement;
 import de.unisaarland.cs.st.reposuite.bugs.tracker.model.Report;
 import de.unisaarland.cs.st.reposuite.exceptions.Shutdown;
 import de.unisaarland.cs.st.reposuite.mapping.model.MapScore;
 import de.unisaarland.cs.st.reposuite.mapping.settings.MappingArguments;
 import de.unisaarland.cs.st.reposuite.mapping.settings.MappingSettings;
+import de.unisaarland.cs.st.reposuite.persistence.model.EnumTuple;
 import de.unisaarland.cs.st.reposuite.rcs.model.RCSTransaction;
-import de.unisaarland.cs.st.reposuite.settings.DoubleArgument;
 import de.unisaarland.cs.st.reposuite.settings.ListArgument;
 import de.unisaarland.cs.st.reposuite.utils.Logger;
 import de.unisaarland.cs.st.reposuite.utils.Regex;
@@ -32,56 +33,18 @@ import de.unisaarland.cs.st.reposuite.utils.RegexGroup;
  */
 public class TimestampEngine extends MappingEngine {
 	
-	private static double                 scoreReportCreatedAfterTransaction   = -10d;
-	private static double                 scoreReportResolvedWithinWindow      = 2d;
-	private static org.joda.time.Interval windowReportResolvedAfterTransaction = new Interval(0, 7200000);
+	private org.joda.time.Interval windowReportResolvedAfterTransaction = new Interval(0, 7200000);
 	
-	/**
-	 * @return the scoreReportCreatedAfterTransaction
-	 */
-	private static double getScoreReportCreatedAfterTransaction() {
-		return scoreReportCreatedAfterTransaction;
-	}
-	
-	/**
-	 * @return the scoreReportResolvedWithinWindow
-	 */
-	private static double getScoreReportResolvedWithinWindow() {
-		return scoreReportResolvedWithinWindow;
+	@Override
+	public String getDescription() {
+		return "Scores based on the relation of close and commit timestamp (1/(1+days(close - upperbound).";
 	}
 	
 	/**
 	 * @return the windowReportResolvedAfterTransaction
 	 */
-	private static org.joda.time.Interval getWindowReportResolvedAfterTransaction() {
-		return windowReportResolvedAfterTransaction;
-	}
-	
-	/**
-	 * @param scoreReportCreatedAfterTransaction the scoreReportCreatedAfterTransaction to set
-	 */
-	private static void setScoreReportCreatedAfterTransaction(final double scoreReportCreatedAfterTransaction) {
-		TimestampEngine.scoreReportCreatedAfterTransaction = scoreReportCreatedAfterTransaction;
-	}
-	
-	/**
-	 * @param scoreReportResolvedWithinWindow the scoreReportResolvedWithinWindow to set
-	 */
-	private static void setScoreReportResolvedWithinWindow(final double scoreReportResolvedAfterTransaction) {
-		TimestampEngine.scoreReportResolvedWithinWindow = scoreReportResolvedAfterTransaction;
-	}
-	
-	/**
-	 * @param windowReportResolvedAfterTransaction the windowReportResolvedAfterTransaction to set
-	 */
-	private static void setWindowReportResolvedAfterTransaction(final org.joda.time.Interval windowReportResolvedAfterTransaction) {
-		TimestampEngine.windowReportResolvedAfterTransaction = windowReportResolvedAfterTransaction;
-	}
-	
-	@Override
-	public String getDescription() {
-		// TODO Auto-generated method stub
-		return "Scores based on the relation of close and commit timestamp (1/(1+days(close - upperbound).";
+	private org.joda.time.Interval getWindowReportResolvedAfterTransaction() {
+		return this.windowReportResolvedAfterTransaction;
 	}
 	
 	/*
@@ -91,10 +54,6 @@ public class TimestampEngine extends MappingEngine {
 	@Override
 	public void init() {
 		super.init();
-		setScoreReportCreatedAfterTransaction((Double) getSettings().getSetting("mapping.score.ReportCreatedAfterTransaction")
-		                                                            .getValue());
-		setScoreReportResolvedWithinWindow((Double) getSettings().getSetting("mapping.score.ReportResolvedWithinWindow")
-		                                                         .getValue());
 		@SuppressWarnings ("unchecked")
 		List<String> list = new LinkedList<String>(
 		                                           (Set<String>) getSettings().getSetting("mapping.window.ReportResolvedAfterTransaction")
@@ -121,8 +80,6 @@ public class TimestampEngine extends MappingEngine {
 		}
 		
 		if (Logger.logInfo()) {
-			Logger.info("Using score 'ReportCreatedAfterTransaction': " + getScoreReportCreatedAfterTransaction());
-			Logger.info("Using score 'ReportResolvedWithinWindow': " + getScoreReportResolvedWithinWindow());
 			Logger.info("Using interval: [" + start + ", " + end + "].");
 		}
 		
@@ -166,14 +123,6 @@ public class TimestampEngine extends MappingEngine {
 	                     final MappingArguments arguments,
 	                     final boolean isRequired) {
 		super.register(settings, arguments, isRequired);
-		arguments.addArgument(new DoubleArgument(settings, "mapping.score.ReportCreatedAfterTransaction",
-		                                         "Score in case the report was created after the transaction.", "-100",
-		                                         isRequired));
-		arguments.addArgument(new DoubleArgument(
-		                                         settings,
-		                                         "mapping.score.ReportResolvedWithinWindow",
-		                                         "Score in case the report was resolved within the specified time window after the transaction.",
-		                                         "2.0", isRequired));
 		arguments.addArgument(new ListArgument(
 		                                       settings,
 		                                       "mapping.window.ReportResolvedAfterTransaction",
@@ -199,57 +148,35 @@ public class TimestampEngine extends MappingEngine {
 		                                 transaction.getTimestamp()
 		                                            .plus(getWindowReportResolvedAfterTransaction().getEndMillis()));
 		
-		if (report.getCreationTimestamp().isBefore(transaction.getTimestamp())) {
-			// report created before transaction
-			
-			if (Logger.logDebug()) {
-				Logger.debug("Report was created before transaction.");
-			}
-			
-			if (!report.getHistory().get(Resolution.class.getSimpleName().toLowerCase()).isEmpty()) {
-				Collection<Report> historicalReports = report.timewarp(interval, Resolution.class.getSimpleName()
-				                                                                                 .toLowerCase());
-				
-				if (!historicalReports.isEmpty()) {
-					for (Report hReport : historicalReports) {
-						if (hReport.getResolution() == Resolution.RESOLVED) {
-							value += getScoreReportResolvedWithinWindow();
-							
-							if (Logger.logDebug()) {
-								Logger.debug("Report was marked as " + Resolution.RESOLVED.name()
-								        + " within the specified time window.");
-							}
-							break;
-						}
-					}
-					
-					if (Logger.logDebug()) {
-						if (Double.compare(value, 0d) == 0) {
-							Logger.debug("Report (created: " + report.getCreationTimestamp() + ", resolved: "
-							        + report.getResolutionTimestamp() + ") hasn't been marked as "
-							        + Resolution.RESOLVED.name() + " within the time window: " + interval);
-						}
-					}
-				} else {
-					if (Logger.logDebug()) {
-						Logger.debug("Report (created: " + report.getCreationTimestamp() + ", resolved: "
-						        + report.getResolutionTimestamp() + ") hasn't been marked as "
-						        + Resolution.RESOLVED.name() + " within the time window: " + interval);
+		if (report.getCreationTimestamp().isBefore(transaction.getTimestamp())
+		        && (report.getResolutionTimestamp() != null)) {
+			History history = report.getHistory().get(Resolution.class.getSimpleName().toLowerCase());
+			for (HistoryElement element : history.getElements()) {
+				EnumTuple tuple = element.getChangedEnumValues().get(Resolution.class.getSimpleName().toLowerCase());
+				@SuppressWarnings ("unchecked")
+				Enum<Resolution> val = (Enum<Resolution>) tuple.getNewValue();
+				if (val.equals(Resolution.RESOLVED)) {
+					if (interval.contains(element.getTimestamp())) {
+						value = 1;
+						
+					} else if (report.getResolutionTimestamp().isAfter(transaction.getTimestamp())) {
+						value = Math.max(value,
+						                 1.0d / (1.0d + (report.getResolutionTimestamp().getMillis() - transaction.getTimestamp()
+						                                                                                          .getMillis()) / 1000d / 3600d / 24d));
 					}
 				}
 			}
-		} else {
 			
-			if (Logger.logDebug()) {
-				Logger.debug("Report created after transaction. Scoring: " + getScoreReportCreatedAfterTransaction());
-			}
-			value += getScoreReportCreatedAfterTransaction();
+			score.addFeature(value, "timestamp", transaction.getTimestamp().toString(), "resolutionTimestamp",
+			                 report.getResolutionTimestamp().toString(), this.getClass());
 		}
-		
-		score.addFeature(value, "timestamp", transaction.getTimestamp().toString(), "resolutionTimestamp",
-		                 report.getResolutionTimestamp() != null
-		                                                        ? report.getResolutionTimestamp().toString()
-		                                                        : "unset", this.getClass());
+	}
+	
+	/**
+	 * @param windowReportResolvedAfterTransaction the windowReportResolvedAfterTransaction to set
+	 */
+	private void setWindowReportResolvedAfterTransaction(final org.joda.time.Interval windowReportResolvedAfterTransaction) {
+		this.windowReportResolvedAfterTransaction = windowReportResolvedAfterTransaction;
 	}
 	
 }
