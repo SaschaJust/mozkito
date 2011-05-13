0f555d642ea28803169b354a550497ede1ce32f0
Kim Herzig <kim@cs.uni-saarland.de>
2011-05-13 16:32:56 +0200

    adapting the LoggerArguments to use the new kiasa log options. Also, not relying on log4j.properties file anymore. Configuration is done internally, only.

--- reposuite-utils/src/main/java/de/unisaarland/cs/st/reposuite/settings/LoggerArguments.java
+++ reposuite-utils/src/main/java/de/unisaarland/cs/st/reposuite/settings/LoggerArguments.java
@@ -1,18 +1,13 @@
 package de.unisaarland.cs.st.reposuite.settings;
 
-import java.net.URL;
-
+import net.ownhero.dev.ioda.FileUtils;
 import net.ownhero.dev.kisa.LogLevel;
-import net.ownhero.dev.kisa.Logger;
 
 /**
  * @author Sascha Just <sascha.just@st.cs.uni-saarland.de>
  * 
  */
 public class LoggerArguments extends RepoSuiteArgumentSet {
-	
-	private final InputFileArgument logConfig;
-	private final EnumArgument      logLevel;
 	
 	/**
 	 * @param settings
@@ -27,9 +22,37 @@ public class LoggerArguments extends RepoSuiteArgumentSet {
 			argEnums[i] = values[i].toString();
 		}
 		
-		this.logLevel = new EnumArgument(settings, "log.level", "determines the log level", "WARN", false, argEnums);
-		this.logConfig = new InputFileArgument(settings, "log.config", "Configuration file for the logging engine",
-		                                       null, false);
+		new EnumArgument(settings, "log.console.level", "determines the log level for the console log", "WARN", false,
+		                 argEnums);
+		new EnumArgument(settings, "log.file.level", "determines the log level for the oevrall log file", "INFO",
+		                 false, argEnums);
+		new OutputFileArgument(settings, "log.file", "specifies the path to a file the file log shall be written to.",
+		                       "./.log", false, true);
+		new StringArgument(
+		                   settings,
+		                   "log.class.<class_name>",
+		                   "This is a special option. You can use also multiple of them. Should be used to specify individual log level for particular classes (also works for categories). Replace <class_name> by the name of the class (full qualified, e.g. net.ownhero.dev.kisa.Logger). The value of the option is the log level to be set for this particula class. Optionally you can also a file path the class log will be written to behind the log level, seperated by a colon. The complete format would look like this:"
+		                           + FileUtils.lineSeparator + "-Dlog.class.<class_name>=<log_level>[,<log_file_path>]",
+		                   null, false);
+		System.setProperty("log.class.org.tmatesoft.svn", "WARN");
+		System.setProperty("log.class.org.hibernate", "WARN");
+		System.setProperty("log.class.org.hibernate.type", "WARN");
+		System.setProperty("log.class.org.apache.http", "WARN");
+		System.setProperty("log.class.org.apache.http.wire", "ERROR");
+		System.setProperty("log.class.openjpa.Tool", "INFO");
+		System.setProperty("log.class.openjpa.Runtime", "INFO");
+		System.setProperty("log.class.openjpa.Remote", "WARN");
+		System.setProperty("log.class.openjpa.DataCache", "WARN");
+		System.setProperty("log.class.openjpa.MetaData", "WARN");
+		System.setProperty("log.class.openjpa.Enhance", "WARN");
+		System.setProperty("log.class.openjpa.Query", "WARN");
+		System.setProperty("log.class.openjpa.jdbc.SQL", "WARN");
+		System.setProperty("log.class.openjpa.jdbc.JDBC", "WARN");
+		System.setProperty("log.class.openjpa.jdbc.Schema", "WARN");
+		// this.logConfig = new InputFileArgument(settings, "log.config",
+		// "Configuration file for the logging engine",
+		// null, false);
+		
 	}
 	
 	/*
@@ -39,18 +62,6 @@ public class LoggerArguments extends RepoSuiteArgumentSet {
 	 */
 	@Override
 	public Boolean getValue() {
-		if (this.logConfig.getValue() != null) {
-			Logger.readConfiguration(this.logConfig.getValue().getAbsolutePath());
-		} else {
-			URL url = Logger.class.getResource("log4j.properties");
-			if (url != null) {
-				Logger.readConfiguration(url);
-			}
-		}
-		
-		if (this.logLevel.getValue() != null) {
-			Logger.setLogLevel(LogLevel.valueOf(this.logLevel.getValue()));
-		}
 		return true;
 	}
 	
