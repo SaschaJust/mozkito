94d683d723ffc51f169b111ee1a6e436cd0a856d
Sascha Just <sascha.just@own-hero.net>
2011-05-26 00:57:50 +0200

    adding bzr2git

---
+++ tools/bzr2git.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+function help() {
+	echo $(basename $0) [TARGET_DIRECTORY] [SOURCE_REPOSITORY]
+}
+
+if [ -z $2 ]; then
+	help
+	exit 1
+fi
+
+TARGET_DIR=$1
+SOURCE_REPO=$2
+
+if [ ${SOURCE_REPO::1} != "/" ]; then
+	SOURCE_REPO="${PWD}/${SOURCE_REPO}"
+fi
+
+if [ ${TARGET_DIR::1} != "/" ]; then
+	TARGET_DIR="${PWD}/${TARGET_DIR}"
+fi
+
+
+## TODO check for darcs executable
+## TODO check for python-fastimport
+
+MYOLDPWD="$PWD"
+
+mkdir -p "${TARGET_DIR}"
+cd "${TARGET_DIR}"
+
+git init --bare
+
+bzr fast-export --plain "${SOURCE_REPO}" | git fast-import
+
+ret=$?
+
+if [ $ret -ne 0 ]; then
+	echo "Converting failed with exit code $?."
+	exit 1
+fi
+
+
+cd "${TARGET_DIR}"
+
+while [ -z $ANS ]; do
+	read -p "Computing deltas and compressing packages. Shall I use aggressive mode? (this takes upto 3G RAM and several minutes to compute) [y|n] " ANS
+	case $ANS in
+		y|Y) GC_OPTIONS="--aggressive"
+			;;
+		n|N) GC_OPTIONS=""
+			;;
+		*) unset ANS
+			;;
+	esac
+done
+
+git gc "$GC_OPTIONS"
+
+if [[ $? -ne 0 ]] ; then
+	if [ -n "${GC_OPTIONS}" ]; then
+		git gc 
+		
+		if [[ $? -eq 0 ]]; then
+			git gc "$GC_OPTIONS"
+			if [[ $? -eq 0 ]]; then
+				echo "Successfully executing garbage collection using '${GC_OPTIONS}'."
+			else 	
+				echo "Successfully executed garbage collection but had to omit '${GC_OPTIONS}'/"
+			fi
+		else
+			echo "Couldn't execute garbage collection at all."
+		fi
+	else
+		echo "Coudn't execute garbage collection."
+	fi
+else
+	echo "Successfully executing garbage collection."
+fi
+
+echo "Converted repository to '${TARGET_DIR}'"
+
+cd "$MYOLDPWD"
