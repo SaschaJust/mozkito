795e2e954b12435981ce85e9620df4ce94913bb3
Sascha Just <sascha.just@own-hero.net>
2011-11-09 14:24:59 +0100

    fixing infozilla not catching unthrown IOException

--- moskito-infozilla/src/main/java/de/unisaarland/cs/st/moskito/infozilla/model/attachment/Attachment.java
+++ moskito-infozilla/src/main/java/de/unisaarland/cs/st/moskito/infozilla/model/attachment/Attachment.java
@@ -183,8 +183,6 @@ public class Attachment implements Annotated {
 			attachment.setAttachable(createAttachable(attachment));
 		} catch (UnsupportedProtocolException e) {
 			throw new FetchException(e);
-		} catch (IOException e) {
-			throw new FetchException(e);
 		}
 		return null;
 	}
