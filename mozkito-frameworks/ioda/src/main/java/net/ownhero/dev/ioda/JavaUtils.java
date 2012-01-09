f7b9495abee3197823bb711a805431b4803f764e
Sascha Just <sascha.just@own-hero.net>
2012-01-09 14:56:29 +0100

    adding documentation

--- mozkito-frameworks/ioda/src/main/java/net/ownhero/dev/ioda/JavaUtils.java
+++ mozkito-frameworks/ioda/src/main/java/net/ownhero/dev/ioda/JavaUtils.java
@@ -21,8 +21,15 @@ public class JavaUtils {
 	        (byte) '5', (byte) '6', (byte) '7', (byte) '8', (byte) '9', (byte) 'a', (byte) 'b', (byte) 'c', (byte) 'd',
 	        (byte) 'e', (byte) 'f'            };
 	
+	/**
+	 * Returns true if any of the submitted objects is null
+	 * 
+	 * @param objects
+	 *            array of objects to be tested for being null
+	 * @return true iff any of the objects in the array is null
+	 */
 	public static boolean AnyNull(final Object... objects) {
-		for (Object object : objects) {
+		for (final Object object : objects) {
 			if (object == null) {
 				return true;
 			}
@@ -31,19 +38,43 @@ public class JavaUtils {
 	}
 	
 	/**
-	 * @param objects
-	 * @return
+	 * Returns a string representing the array with its contents. The
+	 * representation starts with a '[' followed by a comma separated list of
+	 * entries, followed by a ']'. Internally, the array is converted to a list
+	 * and processed by {@link JavaUtils#collectionToString(Collection)}.
+	 * 
+	 * Note that this method may be called recursively, if the entries are or
+	 * contain arrays as well. If an entry is a <code>Collection</code>, a
+	 * <code>Map</code> or an <code>Array</code>, the according
+	 * <code>collectionToString()</code>, <code>mapToString()</code> or
+	 * <code>arrayToString()</code> method is called. Otherwise, we call the
+	 * <code>toString()</code> method of the object.
+	 * 
+	 * @param array
+	 *            the array you request the string representation for
+	 * @return the string representation of the array
 	 */
-	public static String arrayToString(final Object[] objects) {
-		return collectionToString(Arrays.asList(objects));
+	public static String arrayToString(final Object[] array) {
+		return collectionToString(Arrays.asList(array));
 	}
 	
+	/**
+	 * This method converts a <code>byte</code> array to the hexadecimal
+	 * representation in an ASCII encoded string.
+	 * 
+	 * @param raw
+	 *            the byte[] array that shall be converted
+	 * @return an ASCII string containing the hex representation of the byte
+	 *         array
+	 * @throws UnsupportedEncodingException
+	 *             If the 'ASCII' charset is not supported
+	 */
 	public static String byteArrayToHexString(final byte[] raw) throws UnsupportedEncodingException {
-		byte[] hex = new byte[2 * raw.length];
+		final byte[] hex = new byte[2 * raw.length];
 		int index = 0;
 		
-		for (byte b : raw) {
-			int v = b & 0xFF;
+		for (final byte b : raw) {
+			final int v = b & 0xFF;
 			hex[index++] = JavaUtils.HEX_CHAR_TABLE[v >>> 4];
 			hex[index++] = JavaUtils.HEX_CHAR_TABLE[v & 0xF];
 		}
@@ -51,8 +82,18 @@ public class JavaUtils {
 	}
 	
 	/**
+	 * Internal method used by {@link JavaUtils#collectionToString(Collection)},
+	 * {@link JavaUtils#arrayToString(Object[])} and
+	 * {@link JavaUtils#mapToString(Map)}. Checks the entries to implement
+	 * {@link Collection} or {@link Map} or be of type array. In this case the
+	 * according methods in {@link JavaUtils} are called to get their String
+	 * representations. Otherwise, the objects <code>toString()</code> method is
+	 * called.
+	 * 
 	 * @param object
-	 * @return
+	 *            the object that shall be represented as a String
+	 * @return the string representation of the object
+	 * 
 	 */
 	private static String checkDescent(final Object object) {
 		if (object == null) {
@@ -75,24 +116,38 @@ public class JavaUtils {
 			}
 		})) {
 			return mapToString((Map<?, ?>) object);
+		} else if (object.getClass().isArray()) {
+			return arrayToString((Object[]) object);
 		} else {
 			return object.toString();
 		}
 	}
 	
 	/**
+	 * Returns a string representing the collection with its contents. The
+	 * representation starts with a '[' followed by a comma separated list of
+	 * entries, followed by a ']'.
+	 * 
+	 * Note that this method may be called recursively, if the entries are or
+	 * contain collections as well. If an entry is a <code>Collection</code>, a
+	 * <code>Map</code> or an <code>Array</code>, the according
+	 * <code>collectionToString()</code>, <code>mapToString()</code> or
+	 * <code>arrayToString()</code> method is called. Otherwise, we call the
+	 * <code>toString()</code> method of the object.
+	 * 
 	 * @param collection
-	 * @return
+	 *            the collection you request the string representation of
+	 * @return the string representation of the collection
 	 */
 	public static String collectionToString(final Collection<?> collection) {
-		StringBuilder builder = new StringBuilder();
+		final StringBuilder builder = new StringBuilder();
 		
 		if (collection == null) {
 			builder.append("[(null)]");
 		} else {
 			builder.append("[");
 			int i = 0;
-			for (Object object : collection) {
+			for (final Object object : collection) {
 				++i;
 				if (builder.length() > 1) {
 					builder.append(",");
@@ -108,8 +163,12 @@ public class JavaUtils {
 		return builder.toString();
 	}
 	
+	/**
+	 * @param e
+	 * @return
+	 */
 	public static String[] enumToArray(final Enum<?> e) {
-		String[] retval = new String[e.getDeclaringClass().getEnumConstants().length];
+		final String[] retval = new String[e.getDeclaringClass().getEnumConstants().length];
 		
 		for (int i = 0; i < retval.length; ++i) {
 			retval[i] = e.getDeclaringClass().getEnumConstants()[i].toString();
@@ -117,16 +176,24 @@ public class JavaUtils {
 		return retval;
 	}
 	
+	/**
+	 * @param e
+	 * @return
+	 */
 	public static String enumToString(final Enum<?> e) {
-		Object[] enumConstants = e.getDeclaringClass().getEnumConstants();
+		final Object[] enumConstants = e.getDeclaringClass().getEnumConstants();
 		return arrayToString(enumConstants);
 	}
 	
+	/**
+	 * @return
+	 * @throws ClassNotFoundException
+	 */
 	public static Class<?> getCallingClass() throws ClassNotFoundException {
-		Throwable throwable = new Throwable();
+		final Throwable throwable = new Throwable();
 		throwable.fillInStackTrace();
 		
-		StackTraceElement[] stackTrace = throwable.getStackTrace();
+		final StackTraceElement[] stackTrace = throwable.getStackTrace();
 		return Class.forName(stackTrace[1].getClassName());
 	}
 	
@@ -134,10 +201,10 @@ public class JavaUtils {
 	 * @return
 	 */
 	public static String getThisMethodName() {
-		Throwable throwable = new Throwable();
+		final Throwable throwable = new Throwable();
 		throwable.fillInStackTrace();
 		
-		StackTraceElement[] stackTrace = throwable.getStackTrace();
+		final StackTraceElement[] stackTrace = throwable.getStackTrace();
 		return stackTrace[1].getMethodName();
 	}
 	
@@ -146,13 +213,13 @@ public class JavaUtils {
 	 * @return
 	 */
 	public static String mapToString(final Map<?, ?> map) {
-		StringBuilder builder = new StringBuilder();
+		final StringBuilder builder = new StringBuilder();
 		
 		if (map == null) {
 			builder.append("[(null)]");
 		} else {
 			builder.append("[");
-			for (Object key : map.keySet()) {
+			for (final Object key : map.keySet()) {
 				if (builder.length() > 1) {
 					builder.append(",");
 				}
