d3079c380775629b9aa4940c100ec357167cbdf1
Sascha Just <sascha.just@own-hero.net>
2012-01-21 12:35:49 +0100

    adding new getAllClassNames stub that will return of all class names in the class path

--- mozkito-frameworks/ioda/src/main/java/net/ownhero/dev/ioda/ClassFinder.java
+++ mozkito-frameworks/ioda/src/main/java/net/ownhero/dev/ioda/ClassFinder.java
@@ -12,6 +12,7 @@ import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
@@ -78,25 +79,25 @@ public class ClassFinder {
 	                                                 final Integer modifiers) throws ClassNotFoundException,
 	                                                                         WrongClassSearchMethodException,
 	                                                                         IOException {
-		List<String> pathList = new LinkedList<String>();
+		final List<String> pathList = new LinkedList<String>();
 		
 		String classPaths = System.getProperty("java.class.path");
 		String[] split = classPaths.split(System.getProperty("path.separator"));
-		for (String classPath : split) {
+		for (final String classPath : split) {
 			pathList.add(classPath);
 		}
 		classPaths = System.getProperty("reposuiteClassLookup");
 		if (classPaths != null) {
 			split = classPaths.split(System.getProperty("path.separator"));
-			for (String classPath : split) {
+			for (final String classPath : split) {
 				pathList.add(classPath);
 			}
 		}
 		
-		Collection<Class<?>> discoveredClasses = new HashSet<Class<?>>();
-		String thePackage = pakkage.getName();
+		final Collection<Class<?>> discoveredClasses = new HashSet<Class<?>>();
+		final String thePackage = pakkage.getName();
 		
-		for (String classPath : pathList) {
+		for (final String classPath : pathList) {
 			if (classPath.endsWith(".jar")) {
 				discoveredClasses.addAll(getClassesFromJarFile(thePackage, classPath, modifiers));
 			} else {
@@ -104,6 +105,16 @@ public class ClassFinder {
 			}
 		}
 		return discoveredClasses;
+	}
+	
+	/**
+	 * @param classPath
+	 * @return
+	 */
+	public static Set<String> getAllClassNames(final String classPath) {
+		final HashSet<String> classNames = new HashSet<String>();
+		
+		return classNames;
 	}
 	
 	/**
@@ -153,13 +164,13 @@ public class ClassFinder {
 	                                                                          final Integer modifiers) throws ClassNotFoundException,
 	                                                                                                  WrongClassSearchMethodException,
 	                                                                                                  IOException {
-		Collection<Class<?>> discoveredClasses = ClassFinder.getAllClasses(pakkage, modifiers);
+		final Collection<Class<?>> discoveredClasses = ClassFinder.getAllClasses(pakkage, modifiers);
 		
-		Collection<Class<? extends T>> classList = new HashSet<Class<? extends T>>();
-		for (Class<?> discovered : discoveredClasses) {
+		final Collection<Class<? extends T>> classList = new HashSet<Class<? extends T>>();
+		for (final Class<?> discovered : discoveredClasses) {
 			Class<?> aClass = discovered;
 			do {
-				Class<?> tmpClass = aClass.getSuperclass();
+				final Class<?> tmpClass = aClass.getSuperclass();
 				if (tmpClass == null) {
 					// Object case
 					break;
@@ -212,21 +223,21 @@ public class ClassFinder {
 		// This will hold a list of directories matching the packageName. There
 		// may
 		// be more than one if a package is split over multiple jars/paths
-		Collection<File> directories = new HashSet<File>();
+		final Collection<File> directories = new HashSet<File>();
 		
 		// String filePath =
 		// ClassFinder.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-		String path = packageName.replaceAll("\\.", FileUtils.fileSeparator);
+		final String path = packageName.replaceAll("\\.", FileUtils.fileSeparator);
 		
 		try {
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 			
 			if (classLoader == null) {
 				throw new ClassNotFoundException("Can't get class loader.");
 			}
 			
 			// Ask for all resources for the path
-			Enumeration<URL> resources = classLoader.getResources(path);
+			final Enumeration<URL> resources = classLoader.getResources(path);
 			while (resources.hasMoreElements()) {
 				String internalPath = resources.nextElement().getPath();
 				if (internalPath.contains("!")) {
@@ -234,34 +245,34 @@ public class ClassFinder {
 				}
 				
 				if (!internalPath.endsWith(".jar")) {
-					File directory = new File(URLDecoder.decode(internalPath, "UTF-8"));
+					final File directory = new File(URLDecoder.decode(internalPath, "UTF-8"));
 					directories.add(directory);
 					directories.addAll(FileUtils.getRecursiveDirectories(directory));
 				}
 			}
-		} catch (NullPointerException x) {
+		} catch (final NullPointerException x) {
 			throw new ClassNotFoundException(packageName
 			        + " does not appear to be a valid package (Null pointer exception)");
-		} catch (UnsupportedEncodingException encex) {
+		} catch (final UnsupportedEncodingException encex) {
 			throw new ClassNotFoundException(packageName
 			        + " does not appear to be a valid package (Unsupported encoding)");
 		}
 		
-		Collection<Class<?>> classes = new HashSet<Class<?>>();
+		final Collection<Class<?>> classes = new HashSet<Class<?>>();
 		
 		// For every directory identified capture all the .class files
-		for (File directory : directories) {
+		for (final File directory : directories) {
 			if (directory.exists()) {
 				// Get the list of the files contained in the package
-				String[] files = directory.list();
-				for (String file : files) {
+				final String[] files = directory.list();
+				for (final String file : files) {
 					// we are only interested in .class files
 					if (file.endsWith(".class")) {
 						// removes the .class extension
-						int index = directory.getAbsolutePath().indexOf(path);
-						String absolutePackageName = directory.getAbsolutePath().substring(index)
-						                                      .replaceAll(FileUtils.fileSeparator, ".");
-						Class<?> class1 = Class.forName(absolutePackageName + '.'
+						final int index = directory.getAbsolutePath().indexOf(path);
+						final String absolutePackageName = directory.getAbsolutePath().substring(index)
+						                                            .replaceAll(FileUtils.fileSeparator, ".");
+						final Class<?> class1 = Class.forName(absolutePackageName + '.'
 						        + file.substring(0, file.length() - 6));
 						if (modifiers != null) {
 							if ((class1.getModifiers() & modifiers) == 0) {
@@ -313,29 +324,29 @@ public class ClassFinder {
 		// String filePath =
 		// ClassFinder.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 		
-		Collection<Class<?>> classes = new HashSet<Class<?>>();
+		final Collection<Class<?>> classes = new HashSet<Class<?>>();
 		
 		// determine the full qualified pathname string from the package name by
 		// replacing all '.' with OS file separators.
-		String path = packageName.replaceAll("\\.", FileUtils.fileSeparator) + FileUtils.fileSeparator;
+		final String path = packageName.replaceAll("\\.", FileUtils.fileSeparator) + FileUtils.fileSeparator;
 		
 		try {
-			JarFile currentFile = new JarFile(filePath);
+			final JarFile currentFile = new JarFile(filePath);
 			
 			/*
 			 * step through all elements in the jar file and check if there
 			 * exists a class file in given package. If so, load it and add it
 			 * to the collection.
 			 */
-			for (Enumeration<JarEntry> e = currentFile.entries(); e.hasMoreElements();) {
-				JarEntry current = e.nextElement();
+			for (final Enumeration<JarEntry> e = currentFile.entries(); e.hasMoreElements();) {
+				final JarEntry current = e.nextElement();
 				
 				if ((current.getName().length() > path.length())
 				        && current.getName().substring(0, path.length()).equals(path)
 				        && current.getName().endsWith(".class")) {
-					Class<?> class1 = Class.forName(current.getName()
-					                                       .replaceAll(StringEscapeUtils.escapeJava(FileUtils.fileSeparator),
-					                                                   ".").replace(".class", ""));
+					final Class<?> class1 = Class.forName(current.getName()
+					                                             .replaceAll(StringEscapeUtils.escapeJava(FileUtils.fileSeparator),
+					                                                         ".").replace(".class", ""));
 					if (modifiers != null) {
 						if ((class1.getModifiers() & modifiers) == 0) {
 							classes.add(class1);
@@ -345,7 +356,7 @@ public class ClassFinder {
 					}
 				}
 			}
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			if (Logger.logWarn()) {
 				Logger.warn("Skipping invalid JAR file `" + filePath + "`: " + e.getMessage());
 			}
@@ -389,10 +400,10 @@ public class ClassFinder {
 	                                                             final Integer modifiers) throws ClassNotFoundException,
 	                                                                                     WrongClassSearchMethodException,
 	                                                                                     IOException {
-		Collection<Class<?>> discoveredClasses = ClassFinder.getAllClasses(pakkage, modifiers);
+		final Collection<Class<?>> discoveredClasses = ClassFinder.getAllClasses(pakkage, modifiers);
 		
-		Collection<Class<T>> classList = new HashSet<Class<T>>();
-		for (Class<?> discovered : discoveredClasses) {
+		final Collection<Class<T>> classList = new HashSet<Class<T>>();
+		for (final Class<?> discovered : discoveredClasses) {
 			Class<?> aClass = discovered;
 			Class<T> matchingClass = null;
 			
@@ -405,9 +416,9 @@ public class ClassFinder {
 					break;
 				}
 				
-				Class<?>[] classInterfaces = aClass.getInterfaces();
+				final Class<?>[] classInterfaces = aClass.getInterfaces();
 				
-				for (Class<?> classInterface : classInterfaces) {
+				for (final Class<?> classInterface : classInterfaces) {
 					if (classInterface == theInterface) {
 						if (Logger.logTrace()) {
 							Logger.trace("Class implements the " + theInterface.getSimpleName() + " interface: "
@@ -436,8 +447,8 @@ public class ClassFinder {
 						if (Logger.logTrace()) {
 							Logger.trace("Checking superclass : " + aClass.getName() + " for noarg constructor");
 						}
-						Constructor<?>[] cons = aClass.getConstructors();
-						for (Constructor<?> con : cons) {
+						final Constructor<?>[] cons = aClass.getConstructors();
+						for (final Constructor<?> con : cons) {
 							if (con.getTypeParameters().length == 0) {
 								// it will work
 								if (Logger.logTrace()) {
