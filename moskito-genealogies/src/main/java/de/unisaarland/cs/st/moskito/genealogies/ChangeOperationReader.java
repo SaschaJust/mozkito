25e3cc8fbc183c897c10d464509dbd7016283a34
Sascha Just <sascha.just@own-hero.net>
2012-01-24 23:21:29 +0100

    last try or die

--- moskito-genealogies/src/main/java/de/unisaarland/cs/st/moskito/genealogies/ChangeOperationReader.java
+++ moskito-genealogies/src/main/java/de/unisaarland/cs/st/moskito/genealogies/ChangeOperationReader.java
@@ -1,7 +1,10 @@
 package de.unisaarland.cs.st.moskito.genealogies;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.TreeSet;
 
 import net.ownhero.dev.andama.settings.AndamaSettings;
@@ -38,34 +41,50 @@ public class ChangeOperationReader extends AndamaSource<OperationCollection> {
 				                                                                new RCSTransactionTopologicalComparator());
 				TreeSet<RCSTransaction> list_orgid = new TreeSet<RCSTransaction>(
 				                                                                 new RCSTransactionOriginalIDComparator());
+				LinkedList<RCSTransaction> topo_sorted_list = new LinkedList<RCSTransaction>();
+				LinkedList<RCSTransaction> orgid_sorted_list = new LinkedList<RCSTransaction>();
 				
 				for (RCSTransaction transaction : persistenceUtil.load(criteria)) {
 					if (transaction.getBranch().equals(RCSBranch.getMasterBranch())) {
 						list_topo.add(transaction);
 						list_orgid.add(transaction);
+						topo_sorted_list.add(transaction);
+						orgid_sorted_list.add(transaction);
 					}
 				}
+				
+				Collections.sort(topo_sorted_list, new RCSTransactionTopologicalComparator());
+				Collections.sort(orgid_sorted_list, new RCSTransactionTopologicalComparator());
 				
 				RCSTransaction last = null;
 				Iterator<RCSTransaction> it_topo = list_topo.iterator();
 				Iterator<RCSTransaction> it_orgid = list_topo.iterator();
+				ListIterator<RCSTransaction> topo_sorted_it = topo_sorted_list.listIterator();
+				ListIterator<RCSTransaction> orgid_sorted_it = topo_sorted_list.listIterator();
 				
 				while (it_topo.hasNext()) {
 					RCSTransaction transaction_topo = it_topo.next();
 					RCSTransaction transaction_orgid = it_orgid.next();
-					System.err.println("########### " + transaction_topo + " ## " + transaction_orgid);
+					RCSTransaction transaction_topolist = topo_sorted_it.next();
+					RCSTransaction transaction_orgidlist = orgid_sorted_it.next();
 					
 					if (last != null) {
+						System.err.println("Comparing current transaction " + transaction_topo
+						        + " with previous transaction " + last);
 						if (last.compareTo(transaction_topo) != -1) {
 							System.err.println("ERROR: " + last + " vs " + transaction_topo + " WRONG ORDER (-1).");
 						}
 						if (transaction_topo.compareTo(last) != 1) {
 							System.err.println("ERROR: " + transaction_topo + " vs " + last + " WRONG ORDER (1).");
 						}
-						if (!transaction_topo.equals(transaction_orgid)) {
-							System.err.println("ERROR: Got different order (TOPO vs ORGID). TOPO: " + transaction_topo
-							        + " VS ORGID: " + transaction_orgid);
-						}
+					}
+					
+					System.err.println("########### " + transaction_topo + " ## " + transaction_orgid + " ## "
+					        + transaction_topolist + " ## " + transaction_orgidlist);
+					if (!transaction_topo.equals(transaction_orgid) || !transaction_topo.equals(transaction_topolist)
+					        || !transaction_topo.equals(transaction_orgidlist)) {
+						System.err.println("ERROR: Got different order (TOPO vs ORGID). TOPO: " + transaction_topo
+						        + " VS ORGID: " + transaction_orgid);
 					}
 					last = transaction_topo;
 				}
