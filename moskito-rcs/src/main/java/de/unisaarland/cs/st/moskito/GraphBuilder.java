228573ead939f5b5dab0d3d4c93f54600e54e6ed
Kim Herzig <kim@cs.uni-saarland.de>
2012-01-30 18:49:55 +0100

    trying to fix GraphBuilder calling saveOrUpdate for branches when changed

--- moskito-rcs/src/main/java/de/unisaarland/cs/st/moskito/GraphBuilder.java
+++ moskito-rcs/src/main/java/de/unisaarland/cs/st/moskito/GraphBuilder.java
@@ -1,17 +1,14 @@
 /*******************************************************************************
  * Copyright 2011 Kim Herzig, Sascha Just
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 /**
  * 
@@ -42,58 +39,58 @@ import de.unisaarland.cs.st.moskito.settings.RepositorySettings;
  * 
  */
 public class GraphBuilder extends AndamaSink<RCSTransaction> {
-
+	
 	public GraphBuilder(final AndamaGroup threadGroup, final RepositorySettings settings, final Repository repository,
-			final PersistenceUtil persistenceUtil) {
+	        final PersistenceUtil persistenceUtil) {
 		super(threadGroup, settings, false);
 		final Map<String, RevDependency> reverseDependencies = new HashMap<String, RevDependency>();
 		final Map<String, String> latest = new HashMap<String, String>();
 		final Map<String, RCSTransaction> cached = new HashMap<String, RCSTransaction>();
-
+		
 		new PreExecutionHook<RCSTransaction, RCSTransaction>(this) {
-
+			
 			@Override
 			public void preExecution() {
 				if (Logger.logInfo()) {
 					Logger.info("Fetching reverse dependencies. This could take a while...");
 				}
-
-				for (RevDependencyIterator revdep = repository.getRevDependencyIterator(); revdep.hasNext();) {
-					RevDependency rd = revdep.next();
+				
+				for (final RevDependencyIterator revdep = repository.getRevDependencyIterator(); revdep.hasNext();) {
+					final RevDependency rd = revdep.next();
 					reverseDependencies.put(rd.getId(), rd);
 				}
-
+				
 				if (Logger.logInfo()) {
 					Logger.info("Reverse dependencies ready.");
 				}
-
+				
 				persistenceUtil.beginTransaction();
 			}
 		};
-
+		
 		new ProcessHook<RCSTransaction, RCSTransaction>(this) {
-
+			
 			@Override
 			public void process() {
 				if (Logger.logDebug()) {
 					Logger.debug("Updating graph for " + getInputData());
 				}
-
-				RCSTransaction rcsTransaction = getInputData();
-
-				RevDependency revdep = reverseDependencies.get(rcsTransaction.getId());
-				RCSBranch rcsBranch = revdep.getCommitBranch();
+				
+				final RCSTransaction rcsTransaction = getInputData();
+				
+				final RevDependency revdep = reverseDependencies.get(rcsTransaction.getId());
+				final RCSBranch rcsBranch = revdep.getCommitBranch();
 				rcsTransaction.setBranch(rcsBranch);
 				rcsTransaction.addAllTags(revdep.getTagNames());
-				for (String parent : revdep.getParents()) {
+				for (final String parent : revdep.getParents()) {
 					RCSTransaction parentTransaction = null;
 					if (!cached.containsKey(parent)) {
 						try {
 							parentTransaction = persistenceUtil.loadById(parent, RCSTransaction.class);
-						} catch (ArrayIndexOutOfBoundsException e) {
+						} catch (final ArrayIndexOutOfBoundsException e) {
 							throw new UnrecoverableError(
-									"Got child of parent that is not cached an cannot be loaded anymore.",
-									e);
+							                             "Got child of parent that is not cached an cannot be loaded anymore.",
+							                             e);
 						}
 						if (parentTransaction != null) {
 							cached.put(parentTransaction.getId(), parentTransaction);
@@ -106,27 +103,26 @@ public class GraphBuilder extends AndamaSink<RCSTransaction> {
 					} else {
 						if (Logger.logError()) {
 							Logger.error("Got child `" + rcsTransaction.getId()
-									+ "` of unknown parent. This should not happen.");
+							        + "` of unknown parent. This should not happen.");
 						}
 						throw new UnrecoverableError(
-								new UnrecoverableEntryException(
-										"Got child of unknown parent. This should not happen."));
+						                             new UnrecoverableEntryException(
+						                                                             "Got child of unknown parent. This should not happen."));
 					}
 				}
-
+				
 				// detect new branch
 				if (rcsTransaction.getParents().isEmpty()) {
 					rcsTransaction.getBranch().setBegin(rcsTransaction);
 					if (Logger.logDebug()) {
-						Logger.debug("Setting transaction "
-								+ rcsTransaction.getId()
-								+ " as begin transaction of branch "
-								+ rcsTransaction.getBranch().getName());
+						Logger.debug("Setting transaction " + rcsTransaction.getId()
+						        + " as begin transaction of branch " + rcsTransaction.getBranch().getName());
+						persistenceUtil.saveOrUpdate(rcsTransaction.getBranch());
 					}
 				} else {
-					RCSBranch branch = rcsTransaction.getBranch();
+					final RCSBranch branch = rcsTransaction.getBranch();
 					boolean foundParentInBranch = false;
-					for (RCSTransaction parent : rcsTransaction.getParents()) {
+					for (final RCSTransaction parent : rcsTransaction.getParents()) {
 						if (parent.getBranch().equals(branch)) {
 							foundParentInBranch = true;
 							break;
@@ -135,66 +131,66 @@ public class GraphBuilder extends AndamaSink<RCSTransaction> {
 					if (!foundParentInBranch) {
 						branch.setBegin(rcsTransaction);
 						if (Logger.logDebug()) {
-							Logger.debug("Setting transaction "
-									+ rcsTransaction.getId()
-									+ " as begin transaction of branch "
-									+ rcsTransaction.getBranch().getName());
+							Logger.debug("Setting transaction " + rcsTransaction.getId()
+							        + " as begin transaction of branch " + rcsTransaction.getBranch().getName());
 						}
+						persistenceUtil.saveOrUpdate(rcsTransaction.getBranch());
 					}
 				}
-
+				
 				// detect branch merge
 				if (revdep.getParents().size() > 1) {
-					for (String parent : revdep.getParents()) {
-						RCSTransaction parentTransaction = cached.get(parent);
-
+					for (final String parent : revdep.getParents()) {
+						final RCSTransaction parentTransaction = cached.get(parent);
+						
 						if (!parentTransaction.getBranch().getName().equals(rcsTransaction.getBranch().getName())) {
 							// closed branch
 							// remove parent transaction from cache
-
+							
 							parentTransaction.addChild(rcsTransaction);
-
+							
 							// persistenceUtil.update(cached.remove(parentTransaction.getId()));
 							cached.remove(parentTransaction.getId());
 							persistenceUtil.commitTransaction();
 							persistenceUtil.beginTransaction();
 							// remove branch from cache
 							latest.remove(parentTransaction.getBranch().getName());
-
+							
 							parentTransaction.getBranch().setEnd(parentTransaction);
+							persistenceUtil.saveOrUpdate(parentTransaction.getBranch());
 						}
 					}
 				}
-
+				
 				// ++++++ Update caches ++++++
 				// remove old "latest transaction" from cache
 				if (cached.containsKey(latest.get(revdep.getCommitBranch().getName()))) {
 					cached.get(latest.get(revdep.getCommitBranch().getName())).addChild(rcsTransaction);
-
+					
 					// persistenceUtil.update(cached.remove(latest.get(revdep.getCommitBranch().getName())));
 					cached.remove(latest.get(revdep.getCommitBranch().getName()));
 					persistenceUtil.commitTransaction();
 					persistenceUtil.beginTransaction();
 				}
-
+				
 				// add transaction to cache
 				cached.put(rcsTransaction.getId(), rcsTransaction);
-
+				
 				// set new "latest transaction" to active branch cache
 				latest.put(revdep.getCommitBranch().getName(), rcsTransaction.getId());
 				// ------ Update caches ------
 			}
 		};
-
+		
 		new PostExecutionHook<RCSTransaction, RCSTransaction>(this) {
-
+			
 			@Override
 			public void postExecution() {
 				persistenceUtil.commitTransaction();
 				cached.clear();
 			}
 		};
-
+		
 	}
-
+	
 }
