502bf57af7559973c58a78eaff15d7806c99295f
Kim Herzig <kim@cs.uni-saarland.de>
2012-01-30 17:55:39 +0100

    disabling JIRA tracker history parsing. Also added new test files

--- moskito-bugs/src/main/java/de/unisaarland/cs/st/moskito/bugs/tracker/jira/JiraXMLParser.java
+++ moskito-bugs/src/main/java/de/unisaarland/cs/st/moskito/bugs/tracker/jira/JiraXMLParser.java
@@ -15,9 +15,7 @@
  ******************************************************************************/
 package de.unisaarland.cs.st.moskito.bugs.tracker.jira;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -26,9 +24,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import net.ownhero.dev.ioda.DateTimeUtils;
-import net.ownhero.dev.ioda.IOUtils;
-import net.ownhero.dev.ioda.container.RawContent;
-import net.ownhero.dev.ioda.exceptions.FetchException;
 import net.ownhero.dev.ioda.exceptions.UnsupportedProtocolException;
 import net.ownhero.dev.kanuni.annotations.bevahiors.NoneNull;
 import net.ownhero.dev.kanuni.conditions.CompareCondition;
@@ -36,11 +31,9 @@ import net.ownhero.dev.kisa.Logger;
 import net.ownhero.dev.regex.Regex;
 import net.ownhero.dev.regex.RegexGroup;
 
-import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.Namespace;
-import org.jdom.input.SAXBuilder;
 import org.joda.time.DateTime;
 
 import de.unisaarland.cs.st.moskito.bugs.tracker.elements.Priority;
@@ -49,12 +42,11 @@ import de.unisaarland.cs.st.moskito.bugs.tracker.elements.Status;
 import de.unisaarland.cs.st.moskito.bugs.tracker.elements.Type;
 import de.unisaarland.cs.st.moskito.bugs.tracker.model.AttachmentEntry;
 import de.unisaarland.cs.st.moskito.bugs.tracker.model.Comment;
-import de.unisaarland.cs.st.moskito.bugs.tracker.model.HistoryElement;
 import de.unisaarland.cs.st.moskito.bugs.tracker.model.Report;
 import de.unisaarland.cs.st.moskito.persistence.model.Person;
 
 public class JiraXMLParser {
-	
+
 	protected static Regex       idRegex                    = new Regex("^[^-]+-({bugid}\\d+)");
 	// protected static DateTimeFormatter dateTimeFormat =
 	// DateTimeFormat.forPattern("E, dd MMM yyyy HH:mm:ss Z");
@@ -65,12 +57,12 @@ public class JiraXMLParser {
 	protected static final Regex dateTimeHistoryFormatRegex = new Regex(
 			"(({dd}[0-3]\\d)/({MMM}[A-Z][a-z]{2})/({yy}\\d{2})\\s+({hh}[0-1]?\\d):({mm}[0-5]\\d)\\s({a}[AaPp][Mm]))");
 	protected static Namespace   namespace                  = Namespace.getNamespace("http://www.w3.org/1999/xhtml");
-	
+
 	protected static List<AttachmentEntry> extractAttachments(final Element root,
 			final JiraTracker tracker) {
-		
+
 		List<AttachmentEntry> result = new LinkedList<AttachmentEntry>();
-		
+
 		Element element = root.getChild("attachments", root.getNamespace());
 		if (element != null) {
 			@SuppressWarnings ("unchecked")
@@ -78,12 +70,12 @@ public class JiraXMLParser {
 			for (Element attachElem : attachElems) {
 				String attachId = attachElem.getAttributeValue("id");
 				AttachmentEntry attachment = new AttachmentEntry(attachId);
-				
+
 				attachment.setFilename(attachElem.getAttributeValue("name"));
 				try {
 					attachment.setSize(new Long(attachElem.getAttributeValue("size")));
 				} catch (NumberFormatException e) {
-					
+
 				}
 				String attachAuthor = attachElem.getAttributeValue("author");
 				if (new Regex(Regex.emailPattern).matches(attachAuthor)) {
@@ -93,7 +85,7 @@ public class JiraXMLParser {
 				}
 				String attachDate = attachElem.getAttributeValue("created");
 				attachment.setTimestamp(DateTimeUtils.parseDate(attachDate, new Regex(dateTimeFormatRegex.getPattern())));
-				
+
 				String uri = tracker.getUri().toString();
 				if (!uri.endsWith("/")) {
 					uri += "/";
@@ -108,13 +100,13 @@ public class JiraXMLParser {
 						}
 					}
 				}
-				
+
 				result.add(attachment);
 			}
 		}
 		return result;
 	}
-	
+
 	protected static Element getElement(final Element root,
 			final Namespace namespace,
 			final String tag,
@@ -130,7 +122,7 @@ public class JiraXMLParser {
 		throw new NoSuchElementException("Could not find <" + tag + "> tag with attribute `" + attribute + "` set to `"
 				+ value + "` in namespace `" + namespace + "` for parent `" + root.toString() + "`");
 	}
-	
+
 	protected static Priority getPriority(final String prioString) {
 		if (prioString.equals("Blocker")) {
 			return Priority.VERY_HIGH;
@@ -146,7 +138,7 @@ public class JiraXMLParser {
 			return Priority.UNKNOWN;
 		}
 	}
-	
+
 	protected static Resolution getResolution(final String resString) {
 		if (resString.equals("Won't Fix")) {
 			return Resolution.WONT_FIX;
@@ -166,7 +158,7 @@ public class JiraXMLParser {
 			return Resolution.UNKNOWN;
 		}
 	}
-	
+
 	protected static Status getStatus(final String statusString) {
 		if (statusString.equals("Open")) {
 			return Status.VERIFIED;
@@ -192,7 +184,7 @@ public class JiraXMLParser {
 			return Status.UNKNOWN;
 		}
 	}
-	
+
 	private static void handleComments(final List<Element> comments,
 			final Report report) {
 		for (Element comment : comments) {
@@ -205,7 +197,7 @@ public class JiraXMLParser {
 			report.addComment(new Comment(report.getComments().size() + 1, author, commentDate, commentText));
 		}
 	}
-	
+
 	/**
 	 * @param historyUri
 	 * @param report
@@ -223,148 +215,124 @@ public class JiraXMLParser {
 			IOException,
 			SecurityException,
 			NoSuchFieldException {
-		RawContent rawContent = null;
-		try {
-			rawContent = IOUtils.fetch(historyUri);
-			BufferedReader reader = new BufferedReader(new StringReader(rawContent.getContent()));
-			SAXBuilder saxBuilder = new SAXBuilder("org.ccil.cowan.tagsoup.Parser");
-			Document document = saxBuilder.build(reader);
-			reader.close();
-			
-			Element rootElement = document.getRootElement();
-			if (!rootElement.getName().equals("html")) {
-				if (Logger.logError()) {
-					Logger.error("Error while parsing bugzilla report history (id: " + report.getId()
-							+ "). Root element expectedto have `<html>` tag as root element. Got <"
-							+ rootElement.getName() + ">.");
-				}
-				return;
-			}
-			
-			Element body = rootElement.getChild("body", namespace);
-			if (body == null) {
-				if (Logger.logError()) {
-					Logger.error("Error while parsing bugzilla report history (id: " + report.getId()
-							+ "). No <body> tag found.");
-				}
-				return;
-			}
-			
-			Element nextElem = getElement(body, namespace, "div", "id", "main-content");
-			nextElem = getElement(nextElem, namespace, "div", "class", "active-area");
-			nextElem = getElement(nextElem, namespace, "div", "id", "primary");
-			nextElem = getElement(nextElem, namespace, "div", "class", "content");
-			nextElem = getElement(nextElem, namespace, "div", "id", "activitymodule");
-			nextElem = getElement(nextElem, namespace, "div", "class", "mod-content");
-			nextElem = getElement(nextElem, namespace, "div", "id", "issue_actions_container");
-			
-			List<Element> changeHistoryItems = nextElem.getChildren("div", namespace);
-			for (Element changeHistoryItem : changeHistoryItems) {
-				if (changeHistoryItem.getAttributeValue("class").equals("issue-data-block")) {
-					List<Element> actionContainers = changeHistoryItem.getChildren("div", namespace);
-					for (Element actionContainer : actionContainers) {
-						Person author = null;
-						DateTime timestamp = null;
-						String oldValue = null;
-						String newValue = null;
-						
-						Element actionDetails = getElement(actionContainer, namespace, "div", "class", "action-details");
-						
-						List<Element> as = actionDetails.getChildren("a", namespace);
-						if ((as != null) && (as.size() < 2)) {
-							if (Logger.logError()) {
-								Logger.error("Error while parsing jira history (id: " + report.getId()
-										+ "). HTML structure unknown: could not find second a tag in actionDetails");
-							}
-							return;
-						}
-						Element actionDetailsA = as.get(1);
-						String email = actionDetailsA.getAttributeValue("rel");
-						String fullname = actionDetailsA.getText();
-						author = new Person(null, fullname, email);
-						
-						Element date = getElement(actionDetails, namespace, "span", "class", "date");
-						String dateString = date.getText();
-						if (dateString != null) {
-							timestamp = DateTimeUtils.parseDate(dateString, dateTimeHistoryFormatRegex);
-						}
-						
-						Element actionBody = null;
-						try {
-							actionBody = getElement(actionContainer, namespace, "div", "class",
-									"changehistory action-body");
-						} catch (NoSuchElementException e) {
-							actionBody = getElement(actionContainer, namespace, "div", "class", "action-body");
-						}
-						
-						
-						Element table = actionBody.getChild("table", namespace);
-						if (table == null) {
-							if (Logger.logError()) {
-								Logger.error("Error while parsing jira history (id: " + report.getId()
-										+ "). HTML structure unknown: could not find table in actionBody");
-							}
-							return;
-						}
-						
-						Element tbody = table.getChild("tbody", namespace);
-						if (tbody == null) {
-							if (Logger.logError()) {
-								Logger.error("Error while parsing jira history (id: " + report.getId()
-										+ "). HTML structure unknown: could not find tbody in actionBody");
-							}
-							return;
-						}
-						
-						List<Element> trs = tbody.getChildren("tr", namespace);
-						HistoryElement hElement = new HistoryElement(report.getId(), author, timestamp);
-						
-						for (Element tr : trs) {
-							if (tr == null) {
-								if (Logger.logError()) {
-									Logger.error("Error while parsing jira history (id: " + report.getId()
-											+ "). HTML structure unknown: could not find tr in actionBody");
-								}
-								return;
-							}
-							String fieldString = getElement(tr, namespace, "td", "class", "activity-name").getText()
-									.trim();
-							oldValue = getElement(tr, namespace, "td", "class", "activity-old-val").getText().trim();
-							newValue = getElement(tr, namespace, "td", "class", "activity-new-val").getText().trim();
-							
-							if (fieldString.equals("Status")) {
-								hElement.addChangedValue("status", getStatus(oldValue), getStatus(newValue));
-							} else if (fieldString.equals("Resolution")) {
-								hElement.addChangedValue("resolution", getResolution(oldValue), getResolution(newValue));
-							} else if (fieldString.equals("Priority")) {
-								hElement.addChangedValue("priority", getPriority(oldValue), getPriority(newValue));
-							}
-						}
-						if (!hElement.isEmpty()) {
-							report.addHistoryElement(hElement);
-						}
-					}
-				}
-			}
-			
-		} catch (NoSuchElementException e) {
-			if (Logger.logError()) {
-				Logger.error("Error while parsing jira history. HTML structure unknown: " + e.getMessage(), e);
-				if ((rawContent != null) && Logger.logError()) {
-					Logger.error("RAW REPORT DATA:");
-					Logger.error(rawContent.getContent());
-				}
-			}
-			return;
-		} catch (FetchException e) {
-			if (Logger.logError()) {
-				Logger.error("Error while fetching jira history. URL not found: " + e.getMessage(), e);
-			}
-			return;
+
+		// FIXME JIRA-Tracker parse history is broken
+		if (Logger.logWarn()) {
+			Logger.warn("Parsing JIRA history is disabled due to schema changes.");
 		}
-		
+		return;
+
+		/*
+		 * RawContent rawContent = null; try { rawContent =
+		 * IOUtils.fetch(historyUri); BufferedReader reader = new
+		 * BufferedReader(new StringReader(rawContent.getContent())); SAXBuilder
+		 * saxBuilder = new SAXBuilder("org.ccil.cowan.tagsoup.Parser");
+		 * Document document = saxBuilder.build(reader); reader.close();
+		 * 
+		 * Element rootElement = document.getRootElement(); if
+		 * (!rootElement.getName().equals("html")) { if (Logger.logError()) {
+		 * Logger.error("Error while parsing bugzilla report history (id: " +
+		 * report.getId() +
+		 * "). Root element expectedto have `<html>` tag as root element. Got <"
+		 * + rootElement.getName() + ">."); } return; }
+		 * 
+		 * Element body = rootElement.getChild("body", namespace); if (body ==
+		 * null) { if (Logger.logError()) {
+		 * Logger.error("Error while parsing bugzilla report history (id: " +
+		 * report.getId() + "). No <body> tag found."); } return; }
+		 * 
+		 * Element nextElem = getElement(body, namespace, "div", "id",
+		 * "main-content"); nextElem = getElement(nextElem, namespace, "div",
+		 * "class", "active-area"); nextElem = getElement(nextElem, namespace,
+		 * "div", "id", "primary"); nextElem = getElement(nextElem, namespace,
+		 * "div", "class", "content"); nextElem = getElement(nextElem,
+		 * namespace, "div", "id", "activitymodule"); nextElem =
+		 * getElement(nextElem, namespace, "div", "class", "mod-content");
+		 * nextElem = getElement(nextElem, namespace, "div", "id",
+		 * "issue_actions_container");
+		 * 
+		 * List<Element> changeHistoryItems = nextElem.getChildren("div",
+		 * namespace); for (Element changeHistoryItem : changeHistoryItems) { if
+		 * (
+		 * changeHistoryItem.getAttributeValue("class").equals("issue-data-block"
+		 * )) { List<Element> actionContainers =
+		 * changeHistoryItem.getChildren("div", namespace); for (Element
+		 * actionContainer : actionContainers) { Person author = null; DateTime
+		 * timestamp = null; String oldValue = null; String newValue = null;
+		 * 
+		 * Element actionDetails = getElement(actionContainer, namespace, "div",
+		 * "class", "action-details");
+		 * 
+		 * List<Element> as = actionDetails.getChildren("a", namespace); if ((as
+		 * != null) && (as.size() < 2)) { if (Logger.logError()) {
+		 * Logger.error("Error while parsing jira history (id: " +
+		 * report.getId() +
+		 * "). HTML structure unknown: could not find second a tag in actionDetails"
+		 * ); } return; } Element actionDetailsA = as.get(1); String email =
+		 * actionDetailsA.getAttributeValue("rel"); String fullname =
+		 * actionDetailsA.getText(); author = new Person(null, fullname, email);
+		 * 
+		 * Element date = getElement(actionDetails, namespace, "span", "class",
+		 * "date"); String dateString = date.getText(); if (dateString != null)
+		 * { timestamp = DateTimeUtils.parseDate(dateString,
+		 * dateTimeHistoryFormatRegex); }
+		 * 
+		 * Element actionBody = null; try { actionBody =
+		 * getElement(actionContainer, namespace, "div", "class",
+		 * "changehistory action-body"); } catch (NoSuchElementException e) {
+		 * actionBody = getElement(actionContainer, namespace, "div", "class",
+		 * "action-body"); }
+		 * 
+		 * 
+		 * Element table = actionBody.getChild("table", namespace); if (table ==
+		 * null) { if (Logger.logError()) {
+		 * Logger.error("Error while parsing jira history (id: " +
+		 * report.getId() +
+		 * "). HTML structure unknown: could not find table in actionBody"); }
+		 * return; }
+		 * 
+		 * Element tbody = table.getChild("tbody", namespace); if (tbody ==
+		 * null) { if (Logger.logError()) {
+		 * Logger.error("Error while parsing jira history (id: " +
+		 * report.getId() +
+		 * "). HTML structure unknown: could not find tbody in actionBody"); }
+		 * return; }
+		 * 
+		 * List<Element> trs = tbody.getChildren("tr", namespace);
+		 * HistoryElement hElement = new HistoryElement(report.getId(), author,
+		 * timestamp);
+		 * 
+		 * for (Element tr : trs) { if (tr == null) { if (Logger.logError()) {
+		 * Logger.error("Error while parsing jira history (id: " +
+		 * report.getId() +
+		 * "). HTML structure unknown: could not find tr in actionBody"); }
+		 * return; } String fieldString = getElement(tr, namespace, "td",
+		 * "class", "activity-name").getText() .trim(); oldValue =
+		 * getElement(tr, namespace, "td", "class",
+		 * "activity-old-val").getText().trim(); newValue = getElement(tr,
+		 * namespace, "td", "class", "activity-new-val").getText().trim();
+		 * 
+		 * if (fieldString.equals("Status")) {
+		 * hElement.addChangedValue("status", getStatus(oldValue),
+		 * getStatus(newValue)); } else if (fieldString.equals("Resolution")) {
+		 * hElement.addChangedValue("resolution", getResolution(oldValue),
+		 * getResolution(newValue)); } else if (fieldString.equals("Priority"))
+		 * { hElement.addChangedValue("priority", getPriority(oldValue),
+		 * getPriority(newValue)); } } if (!hElement.isEmpty()) {
+		 * report.addHistoryElement(hElement); } } } }
+		 * 
+		 * } catch (NoSuchElementException e) { if (Logger.logError()) {
+		 * Logger.error
+		 * ("Error while parsing jira history. HTML structure unknown: " +
+		 * e.getMessage(), e); if ((rawContent != null) && Logger.logError()) {
+		 * Logger.error("RAW REPORT DATA:");
+		 * Logger.error(rawContent.getContent()); } } return; } catch
+		 * (FetchException e) { if (Logger.logError()) {
+		 * Logger.error("Error while fetching jira history. URL not found: " +
+		 * e.getMessage(), e); } return; }
+		 */
 	}
-	
+
 	/**
 	 * @param elements
 	 * @param report
@@ -397,16 +365,16 @@ public class JiraXMLParser {
 				}
 			}
 		}
-		
+
 	}
-	
+
 	@SuppressWarnings ("unchecked")
 	@NoneNull
 	public static void handleRoot(final Report report,
 			final Element root,
 			final JiraTracker tracker) {
 		CompareCondition.equals(root.getName(), "item", "The root element has to be 'item'.");
-		
+
 		List<Element> children = root.getChildren();
 		for (Element element : children) {
 			if (element.getName().equals("title")) {
