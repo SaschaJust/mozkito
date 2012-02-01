b4461738f46a3e5f8d0d147ddc0f72717a21564f
Kim Herzig <herzig@cs.uni-saarland.de>
2012-02-01 22:39:59 +0100

    minor fixes

--- moskito-genealogies-metrics/src/main/java/de/unisaarland/cs/st/moskito/genealogies/metrics/layer/universal/UniversalStructuralHolesMetrics.java
+++ moskito-genealogies-metrics/src/main/java/de/unisaarland/cs/st/moskito/genealogies/metrics/layer/universal/UniversalStructuralHolesMetrics.java
@@ -1,7 +1,18 @@
+/*******************************************************************************
+ * Copyright 2012 Kim Herzig, Sascha Just
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ ******************************************************************************/
 package de.unisaarland.cs.st.moskito.genealogies.metrics.layer.universal;
 
 import java.util.Collection;
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
 import java.util.LinkedList;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -10,18 +21,17 @@ import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
 import de.unisaarland.cs.st.moskito.genealogies.ChangeGenealogy;
 import de.unisaarland.cs.st.moskito.genealogies.metrics.GenealogyMetricValue;
 
-
 public class UniversalStructuralHolesMetrics<T> {
 	
-	private static String      inEffSize     = "InEffSize";
-	private static String      outEffSize    = "OutEffSize";
-	private static String      effSize       = "EffSize";
-	private static String      inEfficiency  = "InEfficiency";
-	private static String      outEfficiency = "OutEfficiency";
-	private static String      efficiency    = "Efficiency";
+	private static String inEffSize     = "InEffSize";
+	private static String outEffSize    = "OutEffSize";
+	private static String effSize       = "EffSize";
+	private static String inEfficiency  = "InEfficiency";
+	private static String outEfficiency = "OutEfficiency";
+	private static String efficiency    = "Efficiency";
 	
 	public static Collection<String> getMetricNames() {
-		Collection<String> result = new LinkedList<String>();
+		final Collection<String> result = new LinkedList<String>();
 		result.add(inEffSize);
 		result.add(outEffSize);
 		result.add(effSize);
@@ -32,54 +42,55 @@ public class UniversalStructuralHolesMetrics<T> {
 		return result;
 	}
 	
-	private ChangeGenealogy<T> genealogy;
+	private final ChangeGenealogy<T> genealogy;
 	
-	public UniversalStructuralHolesMetrics(ChangeGenealogy<T> genealogy) {
+	public UniversalStructuralHolesMetrics(final ChangeGenealogy<T> genealogy) {
 		this.genealogy = genealogy;
 	}
 	
-	public Collection<GenealogyMetricValue> handle(T node) {
-		Collection<GenealogyMetricValue> result = new LinkedList<GenealogyMetricValue>();
+	public Collection<GenealogyMetricValue> handle(final T node) {
+		final Collection<GenealogyMetricValue> result = new LinkedList<GenealogyMetricValue>();
 		
-		Collection<T> incoming = genealogy.getAllDependants(node);
-		Collection<T> outgoing = genealogy.getAllParents(node);
-		@SuppressWarnings("unchecked") Collection<T> egoNetwork = CollectionUtils.union(incoming, outgoing);
+		final Collection<T> incoming = this.genealogy.getAllDependants(node);
+		final Collection<T> outgoing = this.genealogy.getAllParents(node);
+		@SuppressWarnings ("unchecked")
+		final Collection<T> egoNetwork = CollectionUtils.union(incoming, outgoing);
 		
-		String nodeId = genealogy.getNodeId(node);
+		final String nodeId = this.genealogy.getNodeId(node);
 		
-		DescriptiveStatistics inEffStat = new DescriptiveStatistics();
-		for (T in : incoming) {
-			//get the number of connections between in and all other incomings
-			Collection<T> inTies = genealogy.getAllDependants(in);
-			inTies.addAll(genealogy.getAllParents(in));
+		final DescriptiveStatistics inEffStat = new DescriptiveStatistics();
+		for (final T in : incoming) {
+			// get the number of connections between in and all other incomings
+			final Collection<T> inTies = this.genealogy.getAllDependants(in);
+			inTies.addAll(this.genealogy.getAllParents(in));
 			inEffStat.addValue(CollectionUtils.intersection(incoming, inTies).size());
 		}
-		double inEgoSize = incoming.size();
-		double inEffValue = inEgoSize - inEffStat.getMean();
+		final double inEgoSize = incoming.size();
+		final double inEffValue = inEgoSize - inEffStat.getMean();
 		result.add(new GenealogyMetricValue(inEffSize, nodeId, inEffValue));
 		result.add(new GenealogyMetricValue(inEfficiency, nodeId, (inEffValue / (inEgoSize + 1))));
 		
-		DescriptiveStatistics outEffStat = new DescriptiveStatistics();
-		for (T out : outgoing) {
-			//get the number of connections between in and all other outgoings
-			Collection<T> outTies = genealogy.getAllDependants(out);
-			outTies.addAll(genealogy.getAllParents(out));
+		final DescriptiveStatistics outEffStat = new DescriptiveStatistics();
+		for (final T out : outgoing) {
+			// get the number of connections between in and all other outgoings
+			final Collection<T> outTies = this.genealogy.getAllDependants(out);
+			outTies.addAll(this.genealogy.getAllParents(out));
 			outEffStat.addValue(CollectionUtils.intersection(outgoing, outTies).size());
 		}
-		double outEgoSize = outgoing.size();
-		double outEffValue = outEgoSize - inEffStat.getMean();
+		final double outEgoSize = outgoing.size();
+		final double outEffValue = outEgoSize - inEffStat.getMean();
 		result.add(new GenealogyMetricValue(outEffSize, nodeId, outEffValue));
 		result.add(new GenealogyMetricValue(outEfficiency, nodeId, (outEffValue / (outEgoSize + 1))));
 		
-		DescriptiveStatistics effStat = new DescriptiveStatistics();
-		for (T ego : egoNetwork) {
-			//get the number of connections between in and all other ego-network
-			Collection<T> ties = genealogy.getAllDependants(ego);
-			ties.addAll(genealogy.getAllParents(ego));
+		final DescriptiveStatistics effStat = new DescriptiveStatistics();
+		for (final T ego : egoNetwork) {
+			// get the number of connections between in and all other ego-network
+			final Collection<T> ties = this.genealogy.getAllDependants(ego);
+			ties.addAll(this.genealogy.getAllParents(ego));
 			inEffStat.addValue(CollectionUtils.intersection(egoNetwork, ties).size());
 		}
-		double egoSize = egoNetwork.size();
-		double effValue = egoSize - effStat.getMean();
+		final double egoSize = egoNetwork.size();
+		final double effValue = egoSize - effStat.getMean();
 		result.add(new GenealogyMetricValue(effSize, nodeId, effValue));
 		result.add(new GenealogyMetricValue(efficiency, nodeId, (effValue / (egoSize + 1))));
 		
