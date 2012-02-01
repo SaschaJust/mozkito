0bee33bfa57600425196446381a419da897ed52c
Kim Herzig <herzig@cs.uni-saarland.de>
2012-02-01 22:21:45 +0100

    partly disabling StructuralHoles metrics

--- moskito-genealogies-metrics/src/main/java/de/unisaarland/cs/st/moskito/genealogies/metrics/layer/universal/UniversalStructuralHolesMetrics.java
+++ moskito-genealogies-metrics/src/main/java/de/unisaarland/cs/st/moskito/genealogies/metrics/layer/universal/UniversalStructuralHolesMetrics.java
@@ -27,49 +27,46 @@ import edu.uci.ics.jung.algorithms.metrics.StructuralHoles;
 public class UniversalStructuralHolesMetrics<T> {
 	
 	public static Collection<String> getMetricNames() {
-		Collection<String> result = new LinkedList<String>();
-		result.add(ConstraintName);
-		result.add(aggregateConstraintName);
-		result.add(EffectiveSizeName);
+		final Collection<String> result = new LinkedList<String>();
+		// result.add(ConstraintName);
+		// result.add(aggregateConstraintName);
+		// result.add(EffectiveSizeName);
 		result.add(EfficiencyName);
-		result.add(HierarchyName);
+		// result.add(HierarchyName);
 		return result;
 	}
 	
 	private final ChangeGenealogy<T>                             genealogy;
 	private final StructuralHoles<T, JungGenealogyGraph.Edge<T>> structuralHoles;
 	
-	private static String                                        aggregateConstraintName = "AggregateConstraint";
-	private static String                                        ConstraintName          = "Constraint";
-	private static String                                        EffectiveSizeName       = "EffectiveSize";
-	private static String                                        EfficiencyName          = "Efficiency";
-	private static String                                        HierarchyName           = "Hierarchy";
+	private static String                                        EfficiencyName = "Efficiency";
 	
-	public UniversalStructuralHolesMetrics(ChangeGenealogy<T> genealogy) {
+	public UniversalStructuralHolesMetrics(final ChangeGenealogy<T> genealogy) {
 		this.genealogy = genealogy;
-		JungGenealogyGraph<T> jungGraph = new JungGenealogyGraph<T>(genealogy);
-		structuralHoles = new StructuralHoles<T, JungGenealogyGraph.Edge<T>>(
-		                                                                     jungGraph,
-		                                                                     new Transformer<JungGenealogyGraph.Edge<T>, Integer>() {
-			                                                                     
-			                                                                     @Override
-			                                                                     public Integer transform(Edge<T> input) {
-				                                                                     return 1;
-			                                                                     }
-			                                                                     
-		                                                                     });
+		final JungGenealogyGraph<T> jungGraph = new JungGenealogyGraph<T>(genealogy);
+		this.structuralHoles = new StructuralHoles<T, JungGenealogyGraph.Edge<T>>(
+		                                                                          jungGraph,
+		                                                                          new Transformer<JungGenealogyGraph.Edge<T>, Integer>() {
+			                                                                          
+			                                                                          @Override
+			                                                                          public Integer transform(final Edge<T> input) {
+				                                                                          return 1;
+			                                                                          }
+			                                                                          
+		                                                                          });
 	}
 	
-	public Collection<GenealogyMetricValue> handle(T node) {
-		Collection<GenealogyMetricValue> result = new LinkedList<GenealogyMetricValue>();
+	public Collection<GenealogyMetricValue> handle(final T node) {
+		final Collection<GenealogyMetricValue> result = new LinkedList<GenealogyMetricValue>();
 		
-		String nodeId = genealogy.getNodeId(node);
+		final String nodeId = this.genealogy.getNodeId(node);
 		
-		result.add(new GenealogyMetricValue(aggregateConstraintName, nodeId, structuralHoles.aggregateConstraint(node)));
-		result.add(new GenealogyMetricValue(ConstraintName, nodeId, structuralHoles.constraint(node)));
-		result.add(new GenealogyMetricValue(EffectiveSizeName, nodeId, structuralHoles.effectiveSize(node)));
-		result.add(new GenealogyMetricValue(EfficiencyName, nodeId, structuralHoles.efficiency(node)));
-		result.add(new GenealogyMetricValue(HierarchyName, nodeId, structuralHoles.hierarchy(node)));
+		// result.add(new GenealogyMetricValue(aggregateConstraintName, nodeId,
+		// structuralHoles.aggregateConstraint(node)));
+		// result.add(new GenealogyMetricValue(ConstraintName, nodeId, structuralHoles.constraint(node)));
+		// result.add(new GenealogyMetricValue(EffectiveSizeName, nodeId, structuralHoles.effectiveSize(node)));
+		result.add(new GenealogyMetricValue(EfficiencyName, nodeId, this.structuralHoles.efficiency(node)));
+		// result.add(new GenealogyMetricValue(HierarchyName, nodeId, structuralHoles.hierarchy(node)));
 		
 		return result;
 	}
