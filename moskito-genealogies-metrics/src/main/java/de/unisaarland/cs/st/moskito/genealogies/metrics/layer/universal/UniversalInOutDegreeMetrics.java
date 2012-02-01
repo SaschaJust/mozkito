073fe7168d05d3fa023e1cb1786e2bee78f73749
Kim Herzig <herzig@cs.uni-saarland.de>
2012-02-01 13:56:20 +0100

    making InOutDegree metric simpler

--- moskito-genealogies-metrics/src/main/java/de/unisaarland/cs/st/moskito/genealogies/metrics/layer/universal/UniversalInOutDegreeMetrics.java
+++ moskito-genealogies-metrics/src/main/java/de/unisaarland/cs/st/moskito/genealogies/metrics/layer/universal/UniversalInOutDegreeMetrics.java
@@ -16,44 +16,32 @@ package de.unisaarland.cs.st.moskito.genealogies.metrics.layer.universal;
 import java.util.Collection;
 import java.util.LinkedList;
 
-import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
-
 import de.unisaarland.cs.st.moskito.genealogies.ChangeGenealogy;
 import de.unisaarland.cs.st.moskito.genealogies.metrics.GenealogyMetricValue;
 
 public class UniversalInOutDegreeMetrics<T> {
 	
 	public static Collection<String> getMetricNames() {
-		Collection<String> result = new LinkedList<String>();
-		result.add(avgInDegree);
-		result.add(avgOutDegree);
+		final Collection<String> result = new LinkedList<String>();
+		result.add(inDegree);
+		result.add(outDegree);
 		return result;
 	}
 	
-	private ChangeGenealogy<T> genealogy;
-	private static String      avgInDegree  = "AvgInDegree";
-	private static String      avgOutDegree = "AvgOutDegree";
+	private final ChangeGenealogy<T> genealogy;
+	private static String            inDegree  = "inDegree";
+	private static String            outDegree = "outDegree";
 	
-	public UniversalInOutDegreeMetrics(ChangeGenealogy<T> genealogy) {
+	public UniversalInOutDegreeMetrics(final ChangeGenealogy<T> genealogy) {
 		this.genealogy = genealogy;
 	}
 	
-	public Collection<GenealogyMetricValue> handle(T node) {
-		Collection<GenealogyMetricValue> result = new LinkedList<GenealogyMetricValue>();
-		String nodeId = genealogy.getNodeId(node);
+	public Collection<GenealogyMetricValue> handle(final T node) {
+		final Collection<GenealogyMetricValue> result = new LinkedList<GenealogyMetricValue>();
+		final String nodeId = this.genealogy.getNodeId(node);
 		
-		DescriptiveStatistics outDegreeStat = new DescriptiveStatistics();
-		
-		for (T parent : genealogy.getAllParents(node)) {
-			outDegreeStat.addValue(genealogy.getEdges(node, parent).size());
-		}
-		result.add(new GenealogyMetricValue(avgOutDegree, nodeId, outDegreeStat.getMean()));
-		
-		DescriptiveStatistics inDegreeStat = new DescriptiveStatistics();
-		for (T child : genealogy.getAllDependants(node)) {
-			inDegreeStat.addValue(genealogy.getEdges(child, node).size());
-		}
-		result.add(new GenealogyMetricValue(avgInDegree, nodeId, inDegreeStat.getMean()));
+		result.add(new GenealogyMetricValue(outDegree, nodeId, this.genealogy.outDegree(node)));
+		result.add(new GenealogyMetricValue(inDegree, nodeId, this.genealogy.inDegree(node)));
 		
 		return result;
 	}
