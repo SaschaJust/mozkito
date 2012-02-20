add7e0cfb08a357fffa5cae2dd638c06fa8803b8
Sascha Just <sascha.just@own-hero.net>
2012-02-21 00:43:59 +0100

    Fixing graph builder

---
+++ mozkito-frameworks/andama/src/main/java/net/ownhero/dev/andama/exceptions/InvalidGraphLayoutException.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright 2012 Kim Herzig, Sascha Just
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ ******************************************************************************/
+package net.ownhero.dev.andama.exceptions;
+
+import net.ownhero.dev.kanuni.annotations.simple.NotNull;
+
+/**
+ * @author Sascha Just <sascha.just@st.cs.uni-saarland.de>
+ * 
+ */
+public class InvalidGraphLayoutException extends Exception {
+	
+	/**
+     * 
+     */
+	private static final long serialVersionUID = -5758728046599090552L;
+	
+	public InvalidGraphLayoutException() {
+		super();
+		// PRECONDITIONS
+		
+		try {
+			// TODO Auto-generated constructor stub
+		} finally {
+			// POSTCONDITIONS
+		}
+	}
+	
+	public InvalidGraphLayoutException(@NotNull final String arg0) {
+		super(arg0);
+		// PRECONDITIONS
+		
+		try {
+			// TODO Auto-generated constructor stub
+			
+		} finally {
+			// POSTCONDITIONS
+		}
+	}
+	
+	public InvalidGraphLayoutException(@NotNull final String arg0, @NotNull final Throwable arg1) {
+		super(arg0, arg1);
+		// PRECONDITIONS
+		
+		try {
+			// TODO Auto-generated constructor stub
+		} finally {
+			// POSTCONDITIONS
+		}
+	}
+	
+	public InvalidGraphLayoutException(@NotNull final Throwable arg0) {
+		super(arg0);
+		// PRECONDITIONS
+		
+		try {
+			// TODO Auto-generated constructor stub
+		} finally {
+			// POSTCONDITIONS
+		}
+	}
+	
+}
--- mozkito-frameworks/andama/src/main/java/net/ownhero/dev/andama/threads/Graph.java
+++ mozkito-frameworks/andama/src/main/java/net/ownhero/dev/andama/threads/Graph.java
@@ -18,9 +18,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import net.ownhero.dev.andama.exceptions.InvalidGraphLayoutException;
 import net.ownhero.dev.kanuni.annotations.simple.NotNull;
 import net.ownhero.dev.kanuni.conditions.CollectionCondition;
 import net.ownhero.dev.kanuni.conditions.Condition;
+import net.ownhero.dev.kanuni.conditions.MapCondition;
 
 /**
  * @author Sascha Just <sascha.just@st.cs.uni-saarland.de>
@@ -28,12 +30,12 @@ import net.ownhero.dev.kanuni.conditions.Condition;
  */
 public class Graph {
 	
-	private final HashSet<Class<?>>                                                                                   connectionTypes = new HashSet<Class<?>>();
+	private final HashSet<Class<?>>                                                                connectionTypes = new HashSet<Class<?>>();
 	private final HashMap<Class<?>, HashMap<Class<? extends Node<?, ?>>, LinkedList<INode<?, ?>>>> inputTypes      = new HashMap<Class<?>, HashMap<Class<? extends Node<?, ?>>, LinkedList<INode<?, ?>>>>();
 	
 	private final HashMap<Class<?>, HashMap<Class<? extends Node<?, ?>>, LinkedList<INode<?, ?>>>> outputTypes     = new HashMap<Class<?>, HashMap<Class<? extends Node<?, ?>>, LinkedList<INode<?, ?>>>>();
-	private final List<INode<?, ?>>                                                                        transformers    = new LinkedList<INode<?, ?>>();
-	private Group                                                                                               group;
+	private final List<INode<?, ?>>                                                                transformers    = new LinkedList<INode<?, ?>>();
+	private Group                                                                                  group;
 	
 	/**
 	 * @param group
@@ -59,6 +61,7 @@ public class Graph {
 					
 					map.get(thread.getBaseType()).add(thread);
 				}
+				
 				if (thread.getOutputType() != null) {
 					this.connectionTypes.add(thread.getOutputType());
 					
@@ -87,7 +90,8 @@ public class Graph {
 			                  "outputTypeMap is assumed to be not null within the whole lifespan of this class.");
 			Condition.notNull(this.connectionTypes,
 			                  "connectionTypes is assumed to be not null within the whole lifespan of this class.");
-			// TODO add MapCondition.notEmpty();
+			MapCondition.notEmpty(this.inputTypes, "There has to be at least one connection type in the graph.");
+			MapCondition.notEmpty(this.outputTypes, "There has to be at least one connection type in the graph.");
 			CollectionCondition.notEmpty(this.connectionTypes,
 			                             "There has to be at least one connection type in the graph.");
 			CollectionCondition.maxSize(this.connectionTypes, group.getThreads().size() - 1,
@@ -96,15 +100,20 @@ public class Graph {
 	}
 	
 	/**
+	 * @throws InvalidGraphLayoutException
 	 * 
 	 */
-	public void buildGraph() {
+	public void buildGraph() throws InvalidGraphLayoutException {
 		final List<INode<?, ?>> headThreads = new LinkedList<INode<?, ?>>();
 		final List<INode<?, ?>> tailThreads = new LinkedList<INode<?, ?>>();
+		final Map<Class<?>, Source<?>> orphanedSources = new HashMap<Class<?>, Source<?>>();
+		final Map<Class<?>, Sink<?>> orphanedSinks = new HashMap<Class<?>, Sink<?>>();
 		INode<?, ?> headThread = null;
 		INode<?, ?> tailThread = null;
 		
 		for (final Class<?> inputType : this.connectionTypes) {
+			headThread = null;
+			tailThread = null;
 			final Map<Class<? extends Node<?, ?>>, LinkedList<INode<?, ?>>> inputMap = this.inputTypes.get(inputType);
 			final Map<Class<? extends Node<?, ?>>, LinkedList<INode<?, ?>>> outputMap = this.outputTypes.get(inputType);
 			
@@ -120,14 +129,23 @@ public class Graph {
 			if (inputMap.containsKey(Multiplexer.class)) {
 				final LinkedList<INode<?, ?>> muxList = inputMap.get(Multiplexer.class);
 				if (muxList.size() > 1) {
-					// TODO error
+					throw new InvalidGraphLayoutException(
+					                                      String.format("Found %s %ss with the same input type: %s",
+					                                                    muxList.size(),
+					                                                    Multiplexer.class.getSimpleName().toLowerCase(),
+					                                                    inputType));
 				}
 				
 				if (inputMap.containsKey(Demultiplexer.class)) {
 					// cross
 					final LinkedList<INode<?, ?>> demuxList = inputMap.get(Demultiplexer.class);
 					if (demuxList.size() > 1) {
-						// TODO error
+						throw new InvalidGraphLayoutException(
+						                                      String.format("Found %s %ss with the same input type: %s",
+						                                                    muxList.size(),
+						                                                    Demultiplexer.class.getSimpleName()
+						                                                                       .toLowerCase(),
+						                                                    inputType));
 					} else {
 						// look back for incoming threads (which should only be transformers and sources; since we don't
 						// use transformers in subgraphs, sources is the only relevant thing here).
@@ -145,10 +163,11 @@ public class Graph {
 						
 						headThreads.add(headThread);
 						
-						// next, look to the right for outgoing threads
-						// we are currently in a demux which means we look out for filters next
-						// after that, we take the multiplexer (which definitely exists-checked prior to this)
-						// we then take the available sinks for that type
+						/*
+						 * next, look to the right for outgoing threads we are currently in a demultiplexer which means
+						 * we look out for filters next after that, we take the multiplexer (which definitely
+						 * exists-checked prior to this) we then take the available sinks for that type
+						 */
 						
 						final LinkedList<INode<?, ?>> filterList = inputMap.get(Filter.class);
 						
@@ -163,7 +182,12 @@ public class Graph {
 						// now lookup multiplexers
 						if ((muxList != null) && !muxList.isEmpty()) {
 							if (tailThread.getBaseType().equals(Demultiplexer.class)) {
-								// TODO: ERROR
+								throw new InvalidGraphLayoutException(
+								                                      String.format("Layout would require to connect a %s to a %s. This is invalid.",
+								                                                    Multiplexer.class.getSimpleName()
+								                                                                     .toLowerCase(),
+								                                                    Demultiplexer.class.getSimpleName()
+								                                                                       .toLowerCase()));
 							}
 							
 							for (final INode<?, ?> mux : muxList) {
@@ -226,7 +250,12 @@ public class Graph {
 					// y-close
 					final LinkedList<INode<?, ?>> demuxList = inputMap.get(Demultiplexer.class);
 					if (demuxList.size() > 1) {
-						// TODO error
+						throw new InvalidGraphLayoutException(
+						                                      String.format("Found %s %ss with the same input type: %s",
+						                                                    demuxList.size(),
+						                                                    Demultiplexer.class.getSimpleName()
+						                                                                       .toLowerCase(),
+						                                                    inputType));
 					} else {
 						// look back for incoming threads (which should only be transformers and sources; since we don't
 						// use transformers in subgraphs, sources is the only relevant thing here).
@@ -272,12 +301,23 @@ public class Graph {
 					// flat
 					final LinkedList<INode<?, ?>> sinkList = inputMap.get(Sink.class);
 					if ((sinkList != null) && (sinkList.size() > 1)) {
-						// TODO error
+						throw new InvalidGraphLayoutException(
+						                                      String.format("Found %s %ss but found no corresponding %s with the same input type: %s",
+						                                                    sinkList.size(), Sink.class.getSimpleName()
+						                                                                               .toLowerCase(),
+						                                                    Multiplexer.class.getSimpleName()
+						                                                                     .toLowerCase(), inputType));
 					}
 					
 					final LinkedList<INode<?, ?>> sourceList = outputMap.get(Source.class);
 					if ((sourceList != null) && (sourceList.size() > 1)) {
-						// TODO error
+						throw new InvalidGraphLayoutException(
+						                                      String.format("Found %s %ss but found no corresponding %s with the same input type: %s",
+						                                                    sinkList.size(),
+						                                                    Source.class.getSimpleName().toLowerCase(),
+						                                                    Demultiplexer.class.getSimpleName()
+						                                                                       .toLowerCase(),
+						                                                    inputType));
 					}
 					
 					final LinkedList<INode<?, ?>> filterList = inputMap.get(Filter.class);
@@ -294,18 +334,23 @@ public class Graph {
 					}
 					
 					if ((sourceList != null) && !sourceList.isEmpty()) {
+						final INode<?, ?> source = sourceList.iterator().next();
 						if (headThread != null) {
-							final INode<?, ?> source = sourceList.iterator().next();
 							connect(source, headThread);
 							headThread = null;
+						} else {
+							orphanedSources.put(source.getOutputType(), (Source<?>) source);
 						}
 					}
 					
 					if ((sinkList != null) && !sinkList.isEmpty()) {
+						final INode<?, ?> sink = sinkList.iterator().next();
 						if (tailThread != null) {
-							final INode<?, ?> sink = sinkList.iterator().next();
 							connect(tailThread, sink);
 							tailThread = null;
+						} else {
+							orphanedSinks.put(sink.getInputType(), (Sink<?>) sink);
+							
 						}
 					}
 				}
@@ -315,33 +360,47 @@ public class Graph {
 		for (final INode<?, ?> transformer : this.transformers) {
 			boolean inputConnected = false;
 			boolean outputConnected = false;
+			final Class<?> outputType = transformer.getOutputType();
+			final Class<?> inputType = transformer.getInputType();
+			
 			for (final INode<?, ?> thread : tailThreads) {
 				if (thread.getOutputType().equals(transformer.getInputType())) {
 					connect(thread, transformer);
-					// tailThreads.remove(thread);
 					inputConnected = true;
 					break;
 				}
 			}
 			
+			if (!inputConnected && orphanedSources.containsKey(inputType)) {
+				connect(orphanedSources.get(inputType), transformer);
+				inputConnected = true;
+			}
+			
 			for (final INode<?, ?> thread : headThreads) {
 				if (thread.getInputType().equals(transformer.getOutputType())) {
 					connect(transformer, thread);
-					// headThreads.remove(thread);
 					outputConnected = true;
 					break;
 				}
 			}
 			
+			if (!outputConnected && orphanedSinks.containsKey(outputType)) {
+				connect(transformer, orphanedSinks.get(outputType));
+				outputConnected = true;
+			}
+			
 			if (!inputConnected || !outputConnected) {
-				// TODO error
+				throw new InvalidGraphLayoutException(String.format("The node %d:%s could not be %s connected.",
+				                                                    transformer.getThreadID(), transformer.getName(),
+				                                                    inputConnected
+				                                                                  ? "output"
+				                                                                  : "input"));
 			}
 		}
 		
 		for (final INode<?, ?> thread : this.group.getThreads()) {
 			if (!thread.checkConnections()) {
-				// TODO error
-				System.err.println("ERROR");
+				throw new InvalidGraphLayoutException("Generated graph layout is incomplete.");
 			}
 		}
 	}
