671c7321127d5c9df7650dc7433a7ff9693c5d77
Sascha Just <sascha.just@own-hero.net>
2012-02-20 11:55:05 +0100

    adding remaining terminal colors and making terminalColor enum public

--- mozkito-frameworks/kisa/src/main/java/net/ownhero/dev/kisa/Logger.java
+++ mozkito-frameworks/kisa/src/main/java/net/ownhero/dev/kisa/Logger.java
@@ -121,13 +121,14 @@ public class Logger {
 		
 	}
 	
-	private static enum TerminalColor {
-		RED ("\u001b[0;31m"), YELLOW ("\u001b[0;33m"), NONE ("\u001b[m");
+	public static enum TerminalColor {
+		BLACK ("\u001b[0;30m"), RED ("\u001b[0;31m"), GREEN ("\u001b[0;32m"), YELLOW ("\u001b[0;33m"), BLUE (
+		        "\u001b[0;34m"), MAGENTA ("\u001b[0;35m"), CYAN ("\u001b[0;36m"), WHITE ("\u001b[0;37m"), NONE (
+		        "\u001b[m");
 		
 		public static boolean isSupported() {
 			if (System.getProperty("disableTermColors") == null) {
-				if (System.console() != null) { // avoid colors when piping
-					                            // output
+				if (System.console() != null) { // avoid colors when piping output
 					final String termVariable = System.getenv("TERM");
 					if (termVariable != null) {
 						final Pattern pattern = Pattern.compile(".*color.*", Pattern.CASE_INSENSITIVE);
