d8430e22afb842ac905dd68a14aef2b722e8494d
Kim Herzig <kim@cs.uni-saarland.de>
2012-02-28 21:57:22 +0100

    modifying test

--- moskito-bugs/src/test/java/de/unisaarland/cs/st/moskito/bugs/tracker/model/ReportPersistence_NetTest.java
+++ moskito-bugs/src/test/java/de/unisaarland/cs/st/moskito/bugs/tracker/model/ReportPersistence_NetTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.fail;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import net.ownhero.dev.ioda.FileUtils;
-import net.ownhero.dev.ioda.IOUtils;
 import net.ownhero.dev.ioda.exceptions.FetchException;
 import net.ownhero.dev.ioda.exceptions.UnsupportedProtocolException;
 
@@ -14,10 +12,8 @@ import org.junit.Test;
 
 import de.unisaarland.cs.st.moskito.bugs.exceptions.InvalidParameterException;
 import de.unisaarland.cs.st.moskito.bugs.tracker.RawReport;
-import de.unisaarland.cs.st.moskito.bugs.tracker.Tracker;
 import de.unisaarland.cs.st.moskito.bugs.tracker.XmlReport;
 import de.unisaarland.cs.st.moskito.bugs.tracker.bugzilla.BugzillaTracker;
-import de.unisaarland.cs.st.moskito.bugs.tracker.bugzilla.BugzillaTracker_4_0_4_Test;
 import de.unisaarland.cs.st.moskito.testing.MoskitoTest;
 import de.unisaarland.cs.st.moskito.testing.annotation.DatabaseSettings;
 
@@ -27,19 +23,10 @@ public class ReportPersistence_NetTest extends MoskitoTest {
 	@DatabaseSettings (unit = "bugs")
 	public void testBugzilla() {
 		try {
-			RawReport rawReport = new RawReport(
-			                                    1l,
-			                                    IOUtils.fetch(BugzillaTracker_4_0_4_Test.class.getResource(FileUtils.fileSeparator
-			                                                                                                       + "bugzilla_114562.xml")
-			                                                                                  .toURI()));
 			final BugzillaTracker tracker = new BugzillaTracker();
-			String url = BugzillaTracker_4_0_4_Test.class.getResource(FileUtils.fileSeparator + "bugzilla_114562.xml")
-			                                             .toString();
-			url = url.substring(0, url.lastIndexOf("bugzilla_114562.xml"));
-			final String pattern = "bugzilla_" + Tracker.getBugidplaceholder() + ".xml";
-			
 			try {
-				tracker.setup(new URI(url), null, pattern, null, null, 114562l, 114562l, null);
+				tracker.setup(new URI("https://bugzilla.mozilla.org/"), null, "show_bug.cgi?ctype=xml&id=<BUGID>",
+				              null, null, 444780l, 444780l, null);
 			} catch (final InvalidParameterException e) {
 				e.printStackTrace();
 				fail();
@@ -47,18 +34,18 @@ public class ReportPersistence_NetTest extends MoskitoTest {
 				e.printStackTrace();
 				fail();
 			}
-			rawReport = tracker.fetchSource(tracker.getLinkFromId(114562l));
+			final RawReport rawReport = tracker.fetchSource(tracker.getLinkFromId(444780l));
 			
 			final XmlReport xmlReport = tracker.createDocument(rawReport);
 			final Report report = tracker.parse(xmlReport);
+			// final Report report = new Report(1234l);
+			getPersistenceUtil().beginTransaction();
 			getPersistenceUtil().save(report);
+			getPersistenceUtil().commitTransaction();
 		} catch (final FetchException e) {
 			e.printStackTrace();
 			fail();
 		} catch (final UnsupportedProtocolException e) {
-			e.printStackTrace();
-			fail();
-		} catch (final URISyntaxException e) {
 			e.printStackTrace();
 			fail();
 		}
