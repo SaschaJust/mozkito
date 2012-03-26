0e6271346641810f2a9454382fe5a1f14cf1f6f6
Sascha Just <sascha.just@own-hero.net>
2012-03-26 17:17:21 +0200

    migrating selectors to hiari-0.2 step 1 of 2

--- moskito-mapping/src/main/java/de/unisaarland/cs/st/moskito/mapping/selectors/MappingSelector.java
+++ moskito-mapping/src/main/java/de/unisaarland/cs/st/moskito/mapping/selectors/MappingSelector.java
@@ -12,6 +12,11 @@
  ******************************************************************************/
 package de.unisaarland.cs.st.moskito.mapping.selectors;
 
+import java.io.IOException;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,9 +24,14 @@ import java.util.Set;
 import net.ownhero.dev.hiari.settings.ArgumentSet;
 import net.ownhero.dev.hiari.settings.ArgumentSetOptions;
 import net.ownhero.dev.hiari.settings.IOptions;
+import net.ownhero.dev.hiari.settings.SetArgument;
 import net.ownhero.dev.hiari.settings.exceptions.ArgumentRegistrationException;
 import net.ownhero.dev.hiari.settings.exceptions.SettingsParseError;
+import net.ownhero.dev.hiari.settings.exceptions.UnrecoverableError;
 import net.ownhero.dev.hiari.settings.requirements.Requirement;
+import net.ownhero.dev.ioda.ClassFinder;
+import net.ownhero.dev.ioda.exceptions.WrongClassSearchMethodException;
+import net.ownhero.dev.kisa.Logger;
 import de.unisaarland.cs.st.moskito.mapping.elements.Candidate;
 import de.unisaarland.cs.st.moskito.mapping.mappable.model.MappableEntity;
 import de.unisaarland.cs.st.moskito.mapping.register.Node;
@@ -38,23 +48,19 @@ public abstract class MappingSelector extends Node {
 	
 	static class Options extends ArgumentSetOptions<Set<MappingSelector>, ArgumentSet<Set<MappingSelector>, Options>> {
 		
+		static final String         TAG         = "selectors";                                      //$NON-NLS-1$
+		static final String         DESCRIPTION = Messages.getString("MappingSelector.description"); //$NON-NLS-1$
+		                                                                                             
+		private SetArgument.Options selectorsOption;
+		
 		/**
 		 * @param argumentSet
 		 * @param name
 		 * @param description
 		 * @param requirements
 		 */
-		public Options(final ArgumentSet<?, ?> argumentSet, final String name, final String description,
-		        final Requirement requirements) {
-			super(argumentSet, name, description, requirements);
-			// PRECONDITIONS
-			
-			try {
-				// TODO Auto-generated constructor stub
-				
-			} finally {
-				// POSTCONDITIONS
-			}
+		public Options(final ArgumentSet<?, ?> argumentSet, final Requirement requirements) {
+			super(argumentSet, TAG, DESCRIPTION, requirements);
 		}
 		
 		/*
@@ -64,10 +70,38 @@ public abstract class MappingSelector extends Node {
 		@Override
 		public Set<MappingSelector> init() {
 			// PRECONDITIONS
+			// PRECONDITIONS
+			final Set<MappingSelector> set = new HashSet<MappingSelector>();
 			
 			try {
-				// TODO Auto-generated method stub
-				return null;
+				
+				final SetArgument argument = getSettings().getArgument(this.selectorsOption);
+				final HashSet<String> value = argument.getValue();
+				
+				for (final String name : value) {
+					@SuppressWarnings ("unchecked")
+					final Class<? extends MappingSelector> clazz = (Class<? extends MappingSelector>) Class.forName(name);
+					final MappingSelector instance = clazz.newInstance();
+					instance.init();
+					set.add(instance);
+				}
+				
+				return set;
+			} catch (final ClassNotFoundException e) {
+				if (Logger.logError()) {
+					Logger.error(e.getMessage(), e);
+				}
+				throw new UnrecoverableError(e);
+			} catch (final InstantiationException e) {
+				if (Logger.logError()) {
+					Logger.error(e.getMessage(), e);
+				}
+				throw new UnrecoverableError(e);
+			} catch (final IllegalAccessException e) {
+				if (Logger.logError()) {
+					Logger.error(e.getMessage(), e);
+				}
+				throw new UnrecoverableError(e);
 			} finally {
 				// POSTCONDITIONS
 			}
@@ -84,8 +118,45 @@ public abstract class MappingSelector extends Node {
 			// PRECONDITIONS
 			
 			try {
-				// TODO Auto-generated method stub
-				return null;
+				final Map<String, IOptions<?, ?>> map = new HashMap<String, IOptions<?, ?>>();
+				
+				final HashSet<String> defaultSet = new HashSet<String>();
+				
+				try {
+					final Collection<Class<? extends MappingSelector>> collection = ClassFinder.getClassesExtendingClass(getClass().getPackage(),
+					                                                                                                     MappingSelector.class,
+					                                                                                                     Modifier.ABSTRACT
+					                                                                                                             | Modifier.INTERFACE
+					                                                                                                             | Modifier.PRIVATE
+					                                                                                                             | Modifier.PROTECTED);
+					for (final Class<? extends MappingSelector> c : collection) {
+						defaultSet.add(c.getSimpleName());
+					}
+				} catch (final ClassNotFoundException e) {
+					if (Logger.logError()) {
+						Logger.error(e.getMessage(), e);
+					}
+					
+				} catch (final WrongClassSearchMethodException e) {
+					if (Logger.logError()) {
+						Logger.error(e.getMessage(), e);
+					}
+					
+				} catch (final IOException e) {
+					if (Logger.logError()) {
+						Logger.error(e.getMessage(), e);
+					}
+					
+				}
+				
+				this.selectorsOption = new SetArgument.Options(
+				                                               set,
+				                                               "enabled", Messages.getString("MappingSelector.enabledDescription"), //$NON-NLS-1$ //$NON-NLS-2$ 
+				                                               defaultSet, Requirement.required);
+				
+				map.put(this.selectorsOption.getName(), this.selectorsOption);
+				
+				return map;
 			} finally {
 				// POSTCONDITIONS
 			}
