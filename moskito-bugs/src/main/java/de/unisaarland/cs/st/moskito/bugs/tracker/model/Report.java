712ee4b69128d5c57bb3acdf14d2d11b21490b75
Kim Herzig <kim@cs.uni-saarland.de>
2012-04-27 12:09:53 +0200

    making Report() public and @Deprecated for OpenJPA and EnhancedReport

--- moskito-bugs/src/main/java/de/unisaarland/cs/st/moskito/bugs/tracker/model/Report.java
+++ moskito-bugs/src/main/java/de/unisaarland/cs/st/moskito/bugs/tracker/model/Report.java
@@ -159,7 +159,8 @@ public class Report implements Annotated, Comparable<Report> {
 	/**
 	 * Instantiates a new report.
 	 */
-	private Report() {
+	@Deprecated
+	public Report() {
 		super();
 	}
 	
