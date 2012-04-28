865a19485b65d0c6a09b0cbbb66f2d50ad3e0d08
Sascha Just <sascha.just@own-hero.net>
2012-04-28 11:44:16 +0200

    adding jsucks5 proxy to mozkito-bugs and extending proxy options to support internal and external proxy. this is untested!

--- moskito-bugs/src/main/java/de/unisaarland/cs/st/moskito/bugs/tracker/settings/ProxyOptions.java
+++ moskito-bugs/src/main/java/de/unisaarland/cs/st/moskito/bugs/tracker/settings/ProxyOptions.java
@@ -12,20 +12,35 @@
  *******************************************************************************/
 package de.unisaarland.cs.st.moskito.bugs.tracker.settings;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import net.ownhero.dev.hiari.settings.ArgumentSet;
 import net.ownhero.dev.hiari.settings.ArgumentSetOptions;
+import net.ownhero.dev.hiari.settings.BooleanArgument;
+import net.ownhero.dev.hiari.settings.DirectoryArgument;
+import net.ownhero.dev.hiari.settings.DirectoryArgument.Options;
+import net.ownhero.dev.hiari.settings.HostArgument;
 import net.ownhero.dev.hiari.settings.IOptions;
-import net.ownhero.dev.hiari.settings.LongArgument;
+import net.ownhero.dev.hiari.settings.PortArgument;
 import net.ownhero.dev.hiari.settings.StringArgument;
-import net.ownhero.dev.hiari.settings.StringArgument.Options;
 import net.ownhero.dev.hiari.settings.exceptions.ArgumentRegistrationException;
 import net.ownhero.dev.hiari.settings.exceptions.SettingsParseError;
+import net.ownhero.dev.hiari.settings.exceptions.UnrecoverableError;
 import net.ownhero.dev.hiari.settings.requirements.Requirement;
+import net.ownhero.dev.ioda.FileUtils;
+import net.ownhero.dev.ioda.FileUtils.FileShutdownAction;
+import net.ownhero.dev.ioda.IOUtils;
 import net.ownhero.dev.ioda.ProxyConfig;
 import net.ownhero.dev.kanuni.annotations.bevahiors.NoneNull;
+import net.ownhero.dev.kisa.Logger;
 
 /**
  * The Class MantisOptions.
@@ -35,22 +50,30 @@ import net.ownhero.dev.kanuni.annotations.bevahiors.NoneNull;
 public class ProxyOptions extends ArgumentSetOptions<ProxyConfig, ArgumentSet<ProxyConfig, ProxyOptions>> {
 	
 	/** The host options. */
-	private Options                                             hostOptions;
+	private HostArgument.Options    hostOptions;
 	
 	/** The port options. */
-	private net.ownhero.dev.hiari.settings.LongArgument.Options portOptions;
+	private PortArgument.Options    portOptions;
 	
 	/** The username options. */
-	private Options                                             usernameOptions;
+	private StringArgument.Options  usernameOptions;
 	
 	/** The password options. */
-	private Options                                             passwordOptions;
+	private StringArgument.Options  passwordOptions;
+	
+	private BooleanArgument.Options useProxyOptions;
+	
+	private BooleanArgument.Options internalOptions;
+	
+	private Options                 cacheDirOptions;
 	
 	/**
 	 * Instantiates a new mantis options.
-	 *
-	 * @param argumentSet the argument set
-	 * @param requirement the requirement
+	 * 
+	 * @param argumentSet
+	 *            the argument set
+	 * @param requirement
+	 *            the requirement
 	 */
 	@NoneNull
 	public ProxyOptions(final ArgumentSet<?, ?> argumentSet, final Requirement requirement) {
@@ -67,14 +90,67 @@ public class ProxyOptions extends ArgumentSetOptions<ProxyConfig, ArgumentSet<Pr
 		// PRECONDITIONS
 		
 		try {
-			
-			final StringArgument hostArgument = getSettings().getArgument(this.hostOptions);
-			final LongArgument portArgument = getSettings().getArgument(this.portOptions);
+			final BooleanArgument useProxyArgument = getSettings().getArgument(this.useProxyOptions);
+			final BooleanArgument internalArgument = getSettings().getArgument(this.internalOptions);
+			final HostArgument hostArgument = getSettings().getArgument(this.hostOptions);
+			final PortArgument portArgument = getSettings().getArgument(this.portOptions);
 			final StringArgument usernameArgument = getSettings().getArgument(this.usernameOptions);
 			final StringArgument passwordArgument = getSettings().getArgument(this.passwordOptions);
+			final DirectoryArgument cacheDirArgument = getSettings().getArgument(this.cacheDirOptions);
 			
-			return new ProxyConfig(hostArgument.getValue(), portArgument.getValue().intValue(),
-			                       usernameArgument.getValue(), passwordArgument.getValue());
+			if (useProxyArgument.getValue()) {
+				// use a proxy server
+				if (internalArgument.getValue()) {
+					// use internal proxy
+					if ((hostArgument.getValue() != null) && !hostArgument.getValue().equalsIgnoreCase("localhost")
+					        && !hostArgument.getValue().equalsIgnoreCase("127.0.0.1")) {
+						if (Logger.logWarn()) {
+							Logger.warn("Proxy '%s' argument is invalid. While internal proxy is enabled, we do not support '%s' arguments other than localhost.",
+							            hostArgument.getTag(), hostArgument.getTag());
+						}
+					}
+					
+					// create temporary file to copy JAR to.
+					final File jarFile = FileUtils.createRandomFile("jsucks5-", ".jar", FileShutdownAction.DELETE);
+					
+					try (InputStream inputStream = ProxyOptions.class.getResourceAsStream("jsucks5.jar");
+					        OutputStream outputStream = new FileOutputStream(jarFile)) {
+						// copy JAR to temp file
+						IOUtils.copyInputStream(inputStream, outputStream);
+						final List<String> commandList = new LinkedList<>();
+						// add java binary
+						commandList.add(System.getProperty("java.home") + File.separator + "bin" + File.separator
+						        + "java");
+						commandList.add("-jar");
+						commandList.add(jarFile.getAbsolutePath());
+						commandList.add("" + portArgument.getValue());
+						commandList.add(cacheDirArgument.getValue().getAbsolutePath());
+						
+						final ProcessBuilder builder = new ProcessBuilder(commandList);
+						builder.start();
+						// wait for proxy to settle
+						try {
+							Thread.sleep(1000);
+						} catch (final InterruptedException ignore) {
+							// ignore
+						}
+						
+						// TODO: check if there is a server listening on localhost:port
+					} catch (final IOException e) {
+						throw new UnrecoverableError(e);
+					}
+					
+					return new ProxyConfig(hostArgument.getValue(), portArgument.getValue().intValue(),
+					                       usernameArgument.getValue(), passwordArgument.getValue());
+				} else {
+					// TODO: check if there is a server listening on host:port
+					return new ProxyConfig(hostArgument.getValue(), portArgument.getValue().intValue(),
+					                       usernameArgument.getValue(), passwordArgument.getValue());
+				}
+			} else {
+				return null;
+			}
+			
 		} finally {
 			// POSTCONDITIONS
 		}
@@ -105,29 +181,44 @@ public class ProxyOptions extends ArgumentSetOptions<ProxyConfig, ArgumentSet<Pr
 		
 		try {
 			final Map<String, IOptions<?, ?>> map = new HashMap<String, IOptions<?, ?>>();
-			this.hostOptions = new StringArgument.Options(set, "host", //$NON-NLS-1$
-			                                              Messages.getString("ProxyOptions.proxyHost_description"), //$NON-NLS-1$
-			                                              null, Requirement.optional);
+			this.useProxyOptions = new BooleanArgument.Options(set, "useProxy", "Activates proxy features.", true,
+			                                                   Requirement.optional);
+			req(this.useProxyOptions, map);
+			
+			this.internalOptions = new BooleanArgument.Options(set, "internal", "Use internal proxy (recommended).",
+			                                                   true, Requirement.iff(this.useProxyOptions));
+			req(this.internalOptions, map);
+			
+			this.hostOptions = new HostArgument.Options(set, "host", //$NON-NLS-1$
+			                                            Messages.getString("ProxyOptions.proxyHost_description"), //$NON-NLS-1$
+			                                            null, Requirement.and(Requirement.iff(this.useProxyOptions),
+			                                                                  Requirement.equals(this.internalOptions,
+			                                                                                     false)));
 			req(this.hostOptions, map);
 			
-			this.portOptions = new LongArgument.Options(
+			this.cacheDirOptions = new DirectoryArgument.Options(set, "cacheDir",
+			                                                     "Cache directory for the internal proxy.", null,
+			                                                     Requirement.equals(this.internalOptions, true), true);
+			req(this.cacheDirOptions, map);
+			
+			this.portOptions = new PortArgument.Options(
 			                                            set,
-			                                            "port", Messages.getString("ProxyOptions.proxyPort_description"), null, //$NON-NLS-1$ //$NON-NLS-2$
-			                                            Requirement.iff(this.hostOptions));
+			                                            "port", Messages.getString("ProxyOptions.proxyPort_description"), 8584, //$NON-NLS-1$ //$NON-NLS-2$
+			                                            Requirement.iff(this.useProxyOptions), true);
 			req(this.portOptions, map);
 			
 			this.usernameOptions = new StringArgument.Options(
 			                                                  set,
 			                                                  "username", //$NON-NLS-1$
 			                                                  Messages.getString("ProxyOptions.proxyUser_description"), null, //$NON-NLS-1$
-			                                                  Requirement.iff(this.hostOptions));
+			                                                  Requirement.iff(this.passwordOptions));
 			
 			req(this.usernameOptions, map);
 			this.passwordOptions = new StringArgument.Options(
 			                                                  set,
 			                                                  "password", //$NON-NLS-1$
 			                                                  Messages.getString("ProxyOptions.proxyPassword_description"), //$NON-NLS-1$
-			                                                  null, Requirement.iff(this.hostOptions), true);
+			                                                  null, Requirement.iff(this.usernameOptions), true);
 			
 			req(this.passwordOptions, map);
 			
