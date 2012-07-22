f3d429e39370a260b2911ef91eefedc6c851be4e
Kim Herzig <kim@cs.uni-saarland.de>
2012-07-22 19:51:07 +0200

    adding more debug output to untangling

--- moskito-untangling/src/main/java/de/unisaarland/cs/st/moskito/untangling/aggregation/UntanglingScoreAggregation.java
+++ moskito-untangling/src/main/java/de/unisaarland/cs/st/moskito/untangling/aggregation/UntanglingScoreAggregation.java
@@ -1,17 +1,14 @@
 /*******************************************************************************
  * Copyright 2012 Kim Herzig, Sascha Just
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  *******************************************************************************/
 
 package de.unisaarland.cs.st.moskito.untangling.aggregation;
@@ -42,30 +39,33 @@ import de.unisaarland.cs.st.moskito.untangling.blob.ChangeSet;
 
 /**
  * The Class UntanglingScoreAggregation.
- *
+ * 
  * @author Kim Herzig <herzig@cs.uni-saarland.de>
  */
 public abstract class UntanglingScoreAggregation extends ScoreAggregation<JavaChangeOperation> {
 	
 	/**
 	 * The Enum SampleType.
-	 *
+	 * 
 	 * @author Kim Herzig <herzig@cs.uni-saarland.de>
 	 */
 	public enum SampleType {
 		
 		/** The POSITIVE. */
-		POSITIVE, 
- /** The NEGATIVE. */
- NEGATIVE;
+		POSITIVE,
+		/** The NEGATIVE. */
+		NEGATIVE;
 	}
 	
 	/**
 	 * Gets the samples.
-	 *
-	 * @param transactionSet the transaction set
-	 * @param trainFraction the train fraction
-	 * @param untangling the untangling
+	 * 
+	 * @param transactionSet
+	 *            the transaction set
+	 * @param trainFraction
+	 *            the train fraction
+	 * @param untangling
+	 *            the untangling
 	 * @return the samples
 	 */
 	public Map<SampleType, List<List<Double>>> getSamples(final Collection<ChangeSet> transactionSet,
@@ -76,7 +76,10 @@ public abstract class UntanglingScoreAggregation extends ScoreAggregation<JavaCh
 		final List<ChangeSet> transactions = new ArrayList<ChangeSet>(transactionSet.size());
 		transactions.addAll(transactionSet);
 		
-		// get random 30% of the transactions
+		if (Logger.logDebug()) {
+			Logger.debug("Fetching training samples: %d or %d change sets.", trainFraction, transactions.size());
+		}
+		
 		int numSamples = (int) (transactions.size() * trainFraction);
 		
 		if (Logger.logInfo()) {
