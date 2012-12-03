305a8d7ae81831d07895818c3915e7dcce3c0923
Sascha Just <sascha.just@own-hero.net>
2012-12-03 14:06:01 +0100

    fixing javadoc header

--- mozkito-infozilla/src/main/java/org/mozkito/infozilla/filters/InfozillaFilter.java
+++ mozkito-infozilla/src/main/java/org/mozkito/infozilla/filters/InfozillaFilter.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/***********************************************************************************************************************
  * Copyright 2011 Kim Herzig, Sascha Just
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -9,20 +9,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- ******************************************************************************/
-/**
- * 
- * InfozillaFilter.java
- * 
- * @author Nicolas Bettenburg � 2009-2010, all rights reserved.
- ******************************************************************** 
- *         This file is part of infoZilla. * * InfoZilla is non-free software: you may not redistribute it * and/or
- *         modify it without the permission of the original author. * * InfoZilla is distributed in the hope that it
- *         will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS
- *         FOR A PARTICULAR PURPOSE. *
- ******************************************************************** 
- * 
- */
+ **********************************************************************************************************************/
 
 package org.mozkito.infozilla.filters;
 
