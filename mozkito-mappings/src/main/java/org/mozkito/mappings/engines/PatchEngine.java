df7baa8d0ed0658fb02a523d8813e2975983eb81
Sascha Just <sascha.just@own-hero.net>
2012-12-08 16:21:48 +0100

    Implementing PatchEngine (incomplete). This still requires a distance measure between 2 patches.

--- mozkito-mappings/src/main/java/org/mozkito/mappings/engines/PatchEngine.java
+++ mozkito-mappings/src/main/java/org/mozkito/mappings/engines/PatchEngine.java
@@ -12,8 +12,12 @@
  **********************************************************************************************************************/
 package org.mozkito.mappings.engines;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import net.ownhero.dev.hiari.settings.ArgumentSet;
 import net.ownhero.dev.hiari.settings.ArgumentSetOptions;
@@ -21,11 +25,25 @@ import net.ownhero.dev.hiari.settings.IOptions;
 import net.ownhero.dev.hiari.settings.exceptions.ArgumentRegistrationException;
 import net.ownhero.dev.hiari.settings.exceptions.SettingsParseError;
 import net.ownhero.dev.hiari.settings.requirements.Requirement;
+import net.ownhero.dev.ioda.JavaUtils;
+import difflib.Delta;
 
+import org.mozkito.infozilla.model.EnhancedReport;
+import org.mozkito.infozilla.model.patch.Patch;
 import org.mozkito.mappings.mappable.model.MappableEntity;
+import org.mozkito.mappings.mappable.model.MappableStructuredReport;
+import org.mozkito.mappings.mappable.model.MappableTransaction;
 import org.mozkito.mappings.messages.Messages;
 import org.mozkito.mappings.model.Relation;
+import org.mozkito.mappings.requirements.And;
+import org.mozkito.mappings.requirements.Atom;
 import org.mozkito.mappings.requirements.Expression;
+import org.mozkito.mappings.requirements.Index;
+import org.mozkito.mappings.storages.RepositoryStorage;
+import org.mozkito.mappings.storages.Storage;
+import org.mozkito.versions.Repository;
+import org.mozkito.versions.model.RCSFile;
+import org.mozkito.versions.model.RCSTransaction;
 
 /**
  * The Class PatchEngine.
@@ -118,7 +136,99 @@ public class PatchEngine extends Engine {
 		// PRECONDITIONS
 		
 		try {
+			final MappableStructuredReport mappableStructuredReport = (MappableStructuredReport) from;
+			final EnhancedReport report = mappableStructuredReport.getReport();
+			
+			final MappableTransaction mappableTransaction = (MappableTransaction) to;
+			final RCSTransaction transaction = mappableTransaction.getTransaction();
+			
+			final RepositoryStorage storage = getStorage(RepositoryStorage.class);
+			final Repository repository = storage.getRepository();
+			
+			final Collection<Patch> patches = report.getPatches();
+			
+			final List<String> patchedFilesInReport = new java.util.LinkedList<>();
+			for (final Patch patch : patches) {
+				String originalFile = patch.getOriginalFile();
+				
+				// TODO determine filepath as greatest overlap of OriginalFile() and ModifiedFile()
+				
+				final String[] split = originalFile.split("/|\\\\"); //$NON-NLS-1$
+				if (split.length > 0) {
+					originalFile = split[split.length - 1];
+					patchedFilesInReport.add(originalFile);
+				}
+			}
+			
+			final Collection<RCSFile> changedFiles = transaction.getChangedFiles();
+			final RCSTransaction previousTransaction = transaction.getBranchParent();
+			
+			double localConfidence = 0.0d;
+			
+			for (final RCSFile changedFile : changedFiles) {
+				final String path = changedFile.getPath(previousTransaction);
+				final String[] split = path.split("/"); //$NON-NLS-1$
+				if (split.length > 0) {
+					final String fileName = split[split.length - 1];
+					// TODO determine if filename/path correlates with one in the patchesFiles
+					fileName.equals(fileName);
+					
+					// if so, TODO get the corresponding patch
+					final Patch patch = new Patch();
+					final Collection<Delta> diff = repository.diff(path, previousTransaction.getId(),
+					                                               transaction.getId());
+					
+					// TODO calculate the similarity measure between Patch and Collection<Delta>
+					localConfidence = Math.max(localConfidence, similarity(patch, diff));
+				}
+			}
+			
+			addFeature(score, localConfidence, "PATCH", JavaUtils.collectionToString(patches), //$NON-NLS-1$
+			           "", "DIFF", "", ""); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			
+		} finally {
+			// POSTCONDITIONS
+		}
+	}
+	
+	/**
+	 * @param patch
+	 * @param diff
+	 * @return
+	 */
+	private double similarity(final Patch patch,
+	                          final Collection<Delta> diff) {
+		// PRECONDITIONS
+		
+		try {
 			// TODO Auto-generated method stub
+			// return 0;
+			throw new RuntimeException("Method 'similarity' has not yet been implemented."); //$NON-NLS-1$
+		} finally {
+			// POSTCONDITIONS
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.mozkito.mappings.register.Node#storageDependency()
+	 */
+	@Override
+	public Set<Class<? extends Storage>> storageDependency() {
+		// PRECONDITIONS
+		
+		try {
+			return new HashSet<Class<? extends Storage>>() {
+				
+				/**
+                 * 
+                 */
+				private static final long serialVersionUID = 1L;
+				
+				{
+					add(RepositoryStorage.class);
+				}
+			};
 		} finally {
 			// POSTCONDITIONS
 		}
@@ -133,10 +243,10 @@ public class PatchEngine extends Engine {
 		// PRECONDITIONS
 		
 		try {
-			// TODO Auto-generated method stub
-			return null;
+			return new And(new Atom(Index.FROM, EnhancedReport.class), new Atom(Index.TO, RCSTransaction.class));
 		} finally {
 			// POSTCONDITIONS
 		}
 	}
+	
 }
