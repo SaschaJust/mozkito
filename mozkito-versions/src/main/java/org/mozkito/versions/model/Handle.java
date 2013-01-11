973354f6668e4afea9a54c95f958be14df72d2f4
Kim Herzig <kim@cs.uni-saarland.de>
2013-01-11 11:18:06 +0100

    correction persistence annotations for VersionArchive

--- mozkito-versions/src/main/java/org/mozkito/versions/model/Handle.java
+++ mozkito-versions/src/main/java/org/mozkito/versions/model/Handle.java
@@ -19,14 +19,17 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -90,8 +93,9 @@ public class Handle implements Annotated, Serializable {
 	
 	/**
 	 * Checks if the given transaction changed the underlyign handle.
-	 *
-	 * @param transaction the transaction
+	 * 
+	 * @param transaction
+	 *            the transaction
 	 * @return true, if successful
 	 */
 	@Transient
@@ -131,6 +135,8 @@ public class Handle implements Annotated, Serializable {
 	 * 
 	 * @return the archive
 	 */
+	@ManyToOne (cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH }, fetch = FetchType.LAZY)
+	@Column (nullable = false)
 	public VersionArchive getArchive() {
 		return this.archive;
 	}
