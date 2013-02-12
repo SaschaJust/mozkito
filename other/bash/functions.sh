cbcc810dc77f64463cd65bdebc73c973698b3916
Sascha Just <sascha.just@own-hero.net>
2013-02-12 19:31:33 +0100

    Ok, successfully building a maven site for once. Total time: 1:18:02.787s
    The aggregation isn't properly working yet and produces a lot of dead links so far.

---
+++ other/bash/functions.sh
@@ -0,0 +1,27 @@
+function mvn() {
+	mvn $@ | tee maven.log | grcat conf.mvn
+	rm -f maven.err maven.warn
+	cat maven.log | awk '
+		BEGIN {
+			line=1;
+		}
+		
+		/^\[ERROR\]/ {
+			print "[#"line"]"$0 >>"maven.err";
+		}
+
+		/^\[WARN\]/ {
+			print "[#"line"]"$0 >>"maven.warn";
+		}
+		
+		{
+			++line;
+		}
+	'
+	[ -f maven.warn ] && echo "Detected $(wc -l maven.warn | awk '{ print $1 }') maven warnings. Enter 'mcat ${PWD}/maven.warn' for details."
+	[ -f maven.err ] && echo "Detected $(wc -l maven.err | awk '{ print $1 }') maven errors. Enter 'mcat ${PWD}/maven.err' for details."
+}
+
+function mcat() {
+	cat $@ | grcat conf.mvn
+}
