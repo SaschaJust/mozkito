e85a4a6f2307efa2dd06031248da10dd6249f06a
Sascha Just <sascha.just@own-hero.net>
2013-03-19 17:12:26 +0100

    stub for summarization engine (see ASDS2013)

---
+++ mozkito-modules/mozkito-mappings/src/main/java/org/mozkito/mappings/engines/ClassSummarizationEngine.java
@@ -0,0 +1,110 @@
+/***********************************************************************************************************************
+ * Copyright 2011 Kim Herzig, Sascha Just
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ **********************************************************************************************************************/
+
+package org.mozkito.mappings.engines;
+
+import net.ownhero.dev.kanuni.annotations.simple.NotNull;
+
+import org.mozkito.mappings.model.Relation;
+import org.mozkito.mappings.requirements.Expression;
+
+
+/**
+ * @author Sascha Just <sascha.just@mozkito.org>
+ *
+ */
+public class ClassSummarizationEngine extends Engine {
+	
+	/**
+	 * {@inheritDoc}
+	 * @see org.mozkito.mappings.engines.Engine#score(org.mozkito.mappings.model.Relation)
+	 */
+	@Override
+	public void score(@NotNull Relation relation) {
+		PRECONDITIONS: {
+			// none
+		}
+		
+		try {
+			// TODO Auto-generated method stub
+			// 
+			throw new RuntimeException("Method 'score' has not yet been implemented."); //$NON-NLS-1$
+		} finally {
+			POSTCONDITIONS: {
+				// none
+			}
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see org.mozkito.mappings.engines.Engine#supported()
+	 */
+	@Override
+	public Expression supported() {
+		PRECONDITIONS: {
+			// none
+		}
+		
+		try {
+			// TODO Auto-generated method stub
+			// return null;
+			throw new RuntimeException("Method 'supported' has not yet been implemented."); //$NON-NLS-1$
+		} finally {
+			POSTCONDITIONS: {
+				// none
+			}
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see org.mozkito.mappings.register.Node#getDescription()
+	 */
+	@Override
+	public String getDescription() {
+		PRECONDITIONS: {
+			// none
+		}
+		
+		try {
+			// TODO Auto-generated method stub
+			// return null;
+			throw new RuntimeException("Method 'getDescription' has not yet been implemented."); //$NON-NLS-1$
+		} finally {
+			POSTCONDITIONS: {
+				// none
+			}
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PRECONDITIONS: {
+			// none
+		}
+		
+		try {
+			// TODO Auto-generated method stub
+			// 
+			throw new RuntimeException("Method 'main' has not yet been implemented."); //$NON-NLS-1$
+		} finally {
+			POSTCONDITIONS: {
+				// none
+			}
+		}
+	}
+	
+}
